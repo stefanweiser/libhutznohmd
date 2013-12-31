43dfd66367745369654631bb36c975388e432773
Stefan Weiser <stefan.weiser@bluewin.ch>
2013-12-31 14:27:11 +0100

    Added skeleton for response.

---
+++ src/http/response.hpp
@@ -0,0 +1,38 @@
+/* This file is part of librest.
+ * Copyright (C) 2013 Stefan Weiser
+
+ * The librest project is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3.0 of the License, or (at your
+ * option) any later version.
+
+ * The librest project is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the librest project; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __LIBREST_HTTP_RESPONSE_HPP__
+#define __LIBREST_HTTP_RESPONSE_HPP__
+
+#include <map>
+#include <memory>
+
+#include <socket/listenersocket.hpp>
+
+#include <librest.hpp>
+
+namespace rest
+{
+
+namespace http
+{
+
+} // namespace http
+
+} // namespace rest
+
+#endif // __LIBREST_HTTP_RESPONSE_HPP__
