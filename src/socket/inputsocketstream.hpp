6119057c6914f64041fad4bda30bc8cd68e211f0
Stefan Weiser <stefan.weiser@bluewin.ch>
2014-01-10 20:19:16 +0100

    Added skeleton for socket streams.

---
+++ src/socket/inputsocketstream.hpp
@@ -0,0 +1,31 @@
+/* This file is part of librest.
+ * Copyright (C) 2013 Stefan Weiser
+
+ * The librest project is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3.0 of the License, or (at your
+ * option) any later version.
+
+ * The librest project is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the librest project; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __LIBREST_SOCKET_INPUTSOCKETSTREAM_HPP__
+#define __LIBREST_SOCKET_INPUTSOCKETSTREAM_HPP__
+
+namespace rest
+{
+
+namespace socket
+{
+
+} // namespace socket
+
+} // namespace rest
+
+#endif // __LIBREST_SOCKET_INPUTSOCKETSTREAM_HPP__
