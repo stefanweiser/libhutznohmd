b796d131c956ee15c3c8896b86528df2d91d4f1e
Stefan Weiser <stefan.weiser@bluewin.ch>
2014-03-07 20:43:37 +0100

    Reducing strict overflow flag due to problems with bison.

--- cmake/toolchain.cmake
+++ cmake/toolchain.cmake
@@ -1,4 +1,4 @@
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fPIC -pipe -pthread -std=c99 -D_POSIX_C_SOURCE=200809L -pedantic -Wall -Wbad-function-cast -Wcast-align -Wcast-qual -Werror -Wextra -Wfloat-equal -Wformat=2 -Winline -Wlogical-op -Wnested-externs -Wold-style-definition -Wpointer-arith -Wshadow -Wstrict-aliasing=2 -Wstrict-overflow=5 -Wstrict-prototypes -Wswitch-enum -Wundef -Wunreachable-code")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fPIC -pipe -pthread -std=c99 -D_POSIX_C_SOURCE=200809L -pedantic -Wall -Wbad-function-cast -Wcast-align -Wcast-qual -Werror -Wextra -Wfloat-equal -Wformat=2 -Winline -Wlogical-op -Wnested-externs -Wold-style-definition -Wpointer-arith -Wshadow -Wstrict-aliasing=2 -Wstrict-overflow=1 -Wstrict-prototypes -Wswitch-enum -Wundef -Wunreachable-code")
 set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -O0 -g")
 set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -O2")
 set(CMAKE_C_FLAGS_COVERAGE "${CMAKE_C_FLAGS_DEBUG} --coverage")
