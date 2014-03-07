d2a2d20d44b2a88bab2018e1aacf9143f969bd23
Stefan Weiser <stefan.weiser@bluewin.ch>
2014-03-07 20:49:01 +0100

    Added more performance tests for http parser.

--- examples/performance/main.cpp
+++ examples/performance/main.cpp
@@ -22,11 +22,9 @@
 
 #include <http/parser/lexer.h>
 
-int main()
+void test(const std::string & request)
 {
-    std::cout << "example_performance" << std::endl;
-
-    std::stringstream s("GET / HTTP/1.1\r\n\r\n");
+    std::stringstream s(request);
     std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
     for (size_t i = 0; i < 1000; i++)
     {
@@ -36,8 +34,21 @@ int main()
     }
     std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
     auto diff = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
-    std::cout << std::fixed << std::setprecision(3) << (diff.count() * 1000.0) << " us."
-              << std::endl;
+    std::cout << std::fixed << std::setprecision(3) << (diff.count() * 1000.0)
+              << " us for request: \n" << request << "---\n\n" << std::endl;
+}
 
+int main()
+{
+    std::cout << "example_performance" << std::endl;
+
+    test("GET / HTTP/1.1\r\n\r\n");
+    test("GET /what/a/long/url HTTP/1.1\r\nContent-Length: 0\r\n\r\n");
+    test("GET /what/a/long/url HTTP/1.1\r\nContent-Length: 0\r\nContent-Length: 0\r\n"
+         "Content-Length: 0\r\nContent-Length: 0\r\nContent-Length: 0\r\nContent-Length: 0\r\n"
+         "Content-Length: 0\r\nContent-Length: 0\r\nContent-Length: 0\r\nContent-Length: 0\r\n"
+         "Content-Length: 0\r\nContent-Length: 0\r\nContent-Length: 0\r\nContent-Length: 0\r\n"
+         "Content-Length: 0\r\nContent-Length: 0\r\nContent-Length: 0\r\nContent-Length: 0\r\n"
+         "Content-Length: 0\r\nContent-Length: 0\r\n\r\n");
     return 0;
 }
