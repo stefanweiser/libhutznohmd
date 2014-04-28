6219626570ddef38b92cbbc51ab8b0cd40064c29
Stefan Weiser <stefan.weiser@bluewin.ch>
2014-04-28 20:25:59 +0200

    Merged httpfirstline.cpp and http.cpp.

--- src/http/parser/http.cpp
+++ src/http/parser/http.cpp
@@ -21,7 +21,6 @@
 
 #include <http/parser/utility/charactercompare.hpp>
 #include <http/parser/utility/httpdate.hpp>
-#include <http/parser/utility/httpfirstline.hpp>
 #include <http/parser/utility/trie.hpp>
 
 #include "http.hpp"
@@ -117,6 +116,51 @@ bool parse_headers(int32_t & result, httpscan * scanner)
         result = scanner->lexer_.get();
     }
 
+    return true;
+}
+
+bool lex_request_url(int32_t & character, push_back_string<1000> & url, const lexer & l)
+{
+    do {
+        if ((character < 0) || (false == is_valid_url_character(static_cast<uint8_t>(character)))) {
+            return false;
+        }
+        url.push_back(static_cast<char>(character));
+        character = l.get();
+    } while ((character != ' ') && (character != '\t'));
+    return true;
+}
+
+version lex_http_version(int32_t & character, const lexer & l)
+{
+    character = l.get();
+    if (character == '0') {
+        return rest::http::version::HTTP_1_0;
+    } else if (character == '1') {
+        return rest::http::version::HTTP_1_1;
+    }
+    return version::HTTP_UNKNOWN;
+}
+
+uint16_t lex_status_code(int32_t & result, const lexer & l)
+{
+    int32_t code = l.get_unsigned_integer(result);
+    if (code <= 0) {
+        return 0;
+    }
+    return static_cast<uint16_t>(code);
+}
+
+bool lex_reason_phrase(int32_t & character, push_back_string<100> & phrase, const lexer & l)
+{
+    do {
+        if ((character < 0) ||
+            ((character != ' ') && (character != '\t') && (0 == isalnum(character)))) {
+            return false;
+        }
+        phrase.push_back(static_cast<char>(character));
+        character = l.get();
+    } while (character != '\n');
     return true;
 }
 
