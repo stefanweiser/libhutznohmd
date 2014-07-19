ed6870d3b3d8af4bc98be5f44d5222b24af77b39
Stefan Weiser <stefan.weiser@bluewin.ch>
2014-07-19 20:48:46 +0200

    Added python based make script.

---
+++ make.py
@@ -0,0 +1,296 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+# determine path of the script
+script_path = os.path.dirname(os.path.realpath(__file__))
+python_script_path = os.path.join(script_path, 'python')
+
+# add the python subdirectory to the search path
+sys.path.insert(0, python_script_path)
+
+from argparse import ArgumentParser
+from itertools import chain
+from multiprocessing import cpu_count
+from shutil import rmtree
+from subprocess import call
+
+import tools
+
+
+class Struct:
+    def __init__(self, **args):
+        self.__dict__.update(args)
+
+
+def parse_arguments(steps):
+    parser = ArgumentParser()
+    parser.add_argument('step',
+                        nargs='+')
+
+    mutual_target_group = parser.add_mutually_exclusive_group()
+    mutual_target_group.add_argument('-d',
+                                     '--debug',
+                                     default='debug',
+                                     dest='target',
+                                     action='store_const',
+                                     const='debug',
+                                     help='use debug options for the build')
+    mutual_target_group.add_argument('-r',
+                                     '--release',
+                                     dest='target',
+                                     action='store_const',
+                                     const='release',
+                                     help='use release options for the build')
+
+    step_group = parser.add_argument_group(
+        'build steps')
+
+    for key in sorted(steps):
+        step_group.add_argument(key,
+                                action='store_true',
+                                help=steps[key].help)
+
+    parser.parse_args()
+    return parser.parse_args()
+
+
+build_path = os.path.join(script_path, 'build')
+install_path = os.path.join(script_path, 'install')
+html_path = os.path.join(build_path, 'html')
+
+astyle_rc = os.path.join(script_path, 'astyle.rc')
+source_paths = [os.path.join(script_path, 'examples'),
+                os.path.join(script_path, 'integrationtest'),
+                os.path.join(script_path, 'src'),
+                os.path.join(script_path, 'unittest')]
+
+unittest_path = os.path.join(build_path,
+                             'unittest',
+                             'unittest_restsrv')
+integrationtest_path = os.path.join(build_path,
+                                    'integrationtest',
+                                    'integrationtest_restsrv')
+
+astyle = tools.AStyleTool('astyle', '2.03', astyle_rc)
+cmake = tools.CMakeTool('cmake', '2.8', script_path, build_path, install_path)
+cppcheck = tools.CPPCheckTool('cppcheck', '1.60')
+dot = tools.DotTool('dot', '2.26')
+doxygen = tools.DoxygenTool('doxygen', '1.7.6')
+gcov = tools.GCCTool('gcov', '4.8')
+gpp = tools.GCCTool('g++', '4.8')
+java = tools.JavaTool('java', '1.6')
+lcov = tools.LCOVTool('lcov', 'genhtml', '1.9', build_path)
+lizard = tools.LizardTool('lizard', '1.8.4')
+make = tools.MakeTool('make', '3.81', build_path)
+rats = tools.RATSTool('rats', '2.4', source_paths)
+rpmbuild = tools.RPMBuildTool('rpmbuild', '4.9')
+valgrind = tools.ValgrindTool('valgrind', '3.7.0')
+
+
+class IsNotBootstrappedError(Exception):
+    def __str__(self):
+        return 'ERROR: Unable to find build directory. ' + \
+               'Project is not bootstrapped.'
+
+
+def check_is_bootstrapped():
+    if os.path.exists(build_path) is None:
+        raise IsNotBootstrappedError()
+
+
+def execute_astyle(args):
+    astyle.check_availability()
+    astyle.check_version()
+
+    files = (os.walk(p) for p in source_paths)
+    for root, dirs, files in chain.from_iterable(files):
+        for name in files:
+            filename = os.path.join(root, name)
+            if (filename.endswith('.h') or filename.endswith('.hpp') or
+                    filename.endswith('.c') or filename.endswith('.cpp')):
+                astyle.execute(filename)
+
+
+def execute_bootstrap(args):
+    cmake.check_availability()
+    cmake.check_version()
+
+    os.makedirs(build_path)
+    cmake.execute(args.target)
+
+
+def execute_build(args):
+    gpp.check_availability()
+    gpp.check_version()
+    check_is_bootstrapped()
+
+    make.execute(['-j' + str(cpu_count()), 'all'], build_path)
+
+
+def execute_check(args):
+    cppcheck.check_availability()
+    cppcheck.check_version()
+
+    make.execute(['check'], build_path)
+
+
+def execute_clean(args):
+    try:
+        rmtree(build_path)
+    except FileNotFoundError:
+        pass
+
+    try:
+        rmtree(install_path)
+    except FileNotFoundError:
+        pass
+
+
+def execute_coverage(args):
+    gcov.check_availability()
+    gcov.check_version()
+    lcov.check_availability()
+    lcov.check_version()
+
+    target_path = os.path.join(build_path, 'src')
+    tracefile = os.path.join(target_path, 'coverage.info')
+    lcov_output_path = os.path.join(build_path, 'lcov')
+
+    exec_clean(args)
+    exec_bootstrap(Struct(target='coverage'))
+    exec_build(args)
+
+    lcov.do_basic_trace()
+
+    exec_unittest(args)
+    exec_integrationtest(args)
+
+    lcov.do_test_trace()
+    lcov.generate_coverage()
+
+
+def execute_doc(args):
+    dot.check_availability()
+    dot.check_version()
+    doxygen.check_availability()
+    doxygen.check_version()
+    java.check_availability()
+    java.check_version()
+    check_is_bootstrapped
+
+    plantuml_jar = '/tmp/plantuml.jar'
+    if os.path.exists(plantuml_jar) is False:
+        call(['wget', 'http://sourceforge.net/projects/plantuml/' +
+              'files/plantuml.jar/download', '-O', plantuml_jar])
+
+    call(['java', '-Djava.awt.headless=true', '-jar', plantuml_jar, '-v', '-o',
+          html_path, script_path + '/src/**.(c|cpp|h|hpp)'])
+    make.execute(['doc'], build_path)
+
+
+def execute_install(args):
+    check_is_bootstrapped()
+
+    make.execute(['install'], build_path)
+
+
+def execute_integrationtest(args):
+    check_is_bootstrapped()
+
+    call([integrationtest_path])
+
+
+def execute_lizard(args):
+    lizard.check_availability()
+    lizard.check_version()
+    check_is_bootstrapped()
+
+    make.execute(['lizard'], build_path)
+
+
+def execute_package(args):
+    rpmbuild.check_availability()
+    rpmbuild.check_version()
+    check_is_bootstrapped()
+
+    make.execute(['package'], build_path)
+
+
+def execute_rats(args):
+    rats.check_availability()
+    rats.check_version()
+
+    rats.execute()
+
+
+def execute_unittest(args):
+    check_is_bootstrapped()
+
+    call([unittest_path])
+
+
+def execute_valgrind(args):
+    check_is_bootstrapped()
+
+    valgrind.execute(unittest_path)
+    valgrind.execute(integrationtest_path)
+
+
+def execute_test(args):
+    execute_unittest(args)
+    execute_integrationtest(args)
+
+
+def execute_all(args):
+    execute_clean(args)
+    execute_bootstrap(args)
+    execute_check(args)
+    execute_astyle(args)
+    execute_build(args)
+    execute_unittest(args)
+    execute_integrationtest(args)
+    execute_install(args)
+    execute_package(args)
+
+
+if __name__ == "__main__":
+    steps = {
+        'all': Struct(fn=execute_all,
+                      help='builds all steps to make a package'),
+        'astyle': Struct(fn=execute_astyle,
+                         help='formats all sources'),
+        'bootstrap': Struct(fn=execute_bootstrap,
+                            help='bootstraps the build'),
+        'build': Struct(fn=execute_build,
+                        help='compiles the targets'),
+        'check': Struct(fn=execute_check,
+                        help='checks all sources'),
+        'clean': Struct(fn=execute_clean,
+                        help='removes all built output'),
+        'coverage': Struct(fn=execute_coverage,
+                           help='generates lcov output'),
+        'doc': Struct(fn=execute_doc,
+                      help='compiles documentation'),
+        'install': Struct(fn=execute_install,
+                          help='installs the targets'),
+        'integrationtest': Struct(fn=execute_integrationtest,
+                                  help='executes integration tests'),
+        'lizard': Struct(fn=execute_lizard,
+                         help='checks for cyclic complexity violations'),
+        'package': Struct(fn=execute_package,
+                          help='builds packages'),
+        'rats': Struct(fn=execute_rats,
+                       help='searches for security issues'),
+        'test': Struct(fn=execute_test,
+                       help='executes unit and integration tests'),
+        'unittest': Struct(fn=execute_unittest,
+                           help='executes unit tests'),
+        'valgrind': Struct(fn=execute_valgrind,
+                           help='searches for memory leaks')
+    }
+
+    args = parse_arguments(steps)
+    for step in args.step:
+        steps[step].fn(args)
---
+++ python/tools.py
@@ -0,0 +1,228 @@
+#!/usr/bin/env python3
+
+from locale import getdefaultlocale
+from os.path import join
+from shutil import which
+from subprocess import call, PIPE, Popen
+
+
+class ToolNotAvailableError(Exception):
+    def __init__(self, tool_name):
+        self.tool_name = tool_name
+
+    def __str__(self):
+        return 'ERROR: Unable to find program ' + self.tool_name + '.'
+
+
+class VersionDoesNotFitError(Exception):
+    def __init__(self, tool_name, min_version, found_version):
+        self.tool_name = tool_name
+        self.min_version = min_version
+        self.found_version = found_version
+
+    def __str__(self):
+        return 'ERROR: Need at least version ' + min_version + \
+               ' of program ' + command + ', but only ' + found_version + \
+               ' was found.'
+
+
+class Tool:
+    def __init__(self, executable_name, minimum_version):
+        self.executable_name = executable_name
+        self.minimum_version = minimum_version.split('.')
+
+    def get_version(self):
+        pass
+
+    def check_availability(self):
+        if which(self.executable_name) is None:
+            raise ToolNotAvailableError(self.executable_name)
+
+    def check_version(self):
+        found_version = self.get_version()
+        for i, v in enumerate(self.minimum_version):
+            if found_version[i] < v:
+                VersionDoesNotFitError(self.executable_name,
+                                       self.minimum_version,
+                                       found_version)
+            elif found_version[i] > v:
+                break
+
+        print('INFO: Fulfilling need for version ' +
+              '.'.join(self.minimum_version) +
+              ' of program ' + self.executable_name + ' with version ' +
+              '.'.join(found_version) + '.')
+
+
+def get_command_output(command):
+    encoding = getdefaultlocale()[1]
+    p = Popen(command, stdout=PIPE, stderr=PIPE)
+    result = p.communicate()
+    return [result[0].decode(encoding), result[1].decode(encoding)]
+
+
+class AStyleTool(Tool):
+    def __init__(self, executable_name, minimum_version, options_path):
+        Tool.__init__(self, executable_name, minimum_version)
+        self.options_path = options_path
+
+    def get_version(self):
+        s = get_command_output([self.executable_name, '--version'])
+        return s[1].strip().split()[-1].split('.')
+
+    def execute(self, filename):
+        call([self.executable_name, '--options=' + self.options_path,
+              filename])
+
+
+class CMakeTool(Tool):
+    def __init__(self, executable_name, minimum_version, script_path,
+                 build_path, install_path):
+        Tool.__init__(self, executable_name, minimum_version)
+        self.script_path = script_path
+        self.build_path = build_path
+        self.install_path = install_path
+
+    def get_version(self):
+        s = get_command_output([self.executable_name, '--version'])
+        return s[0].strip().split()[-1].split('.')
+
+    def execute(self, target):
+        call([self.executable_name, self.script_path,
+              '-DCMAKE_INSTALL_PREFIX=' + self.install_path,
+              '-DCMAKE_BUILD_TYPE=' + target], cwd=self.build_path)
+
+
+class CPPCheckTool(Tool):
+    def __init__(self, executable_name, minimum_version):
+        Tool.__init__(self, executable_name, minimum_version)
+
+    def get_version(self):
+        s = get_command_output([self.executable_name, '--version'])
+        return s[0].split()[-1].split('.')
+
+
+class DoxygenTool(Tool):
+    def __init__(self, executable_name, minimum_version):
+        Tool.__init__(self, executable_name, minimum_version)
+
+    def get_version(self):
+        s = get_command_output([self.executable_name, '--version'])
+        return s[0].strip().split('.')
+
+
+class DotTool(Tool):
+    def __init__(self, executable_name, minimum_version):
+        Tool.__init__(self, executable_name, minimum_version)
+
+    def get_version(self):
+        s = get_command_output([self.executable_name, '-V'])
+        return s[1].strip().split()[-2].split('.')
+
+
+class GCCTool(Tool):
+    def __init__(self, executable_name, minimum_version):
+        Tool.__init__(self, executable_name, minimum_version)
+
+    def get_version(self):
+        s = get_command_output([self.executable_name, '--version'])
+        return s[0].split('\n')[0].split()[-1].split('.')
+
+
+class JavaTool(Tool):
+    def __init__(self, executable_name, minimum_version):
+        Tool.__init__(self, executable_name, minimum_version)
+
+    def get_version(self):
+        s = get_command_output([self.executable_name, '-version'])
+        return s[1].split('\n')[0].split()[-1][1:-1].split('.')
+
+
+class LCOVTool(Tool):
+    def __init__(self, executable_name, html_generator_name, minimum_version,
+                 build_path):
+        Tool.__init__(self, executable_name, minimum_version)
+        self.html_generator_name = html_generator_name
+        self.target_path = join(build_path, 'src')
+        self.tracefile = join(self.target_path, 'coverage.info')
+        self.output_path = join(build_path, 'lcov')
+
+    def get_version(self):
+        s = get_command_output([self.executable_name, '--version'])
+        return s[0].strip().split()[-1].split('.')
+
+    def do_basic_trace(self):
+        call([self.executable_name, '-c', '-i', '-d', self.target_path, '-o',
+              self.tracefile + '.base'], cwd=self.build_path)
+
+    def do_test_trace(self):
+        call([self.executable_name, '-c', '-d', self.target_path, '-o',
+              self.tracefile + '.test'], cwd=self.build_path)
+
+    def generate_coverage(self):
+        call([self.executable_name, '-c', '-d', self.target_path, '-o',
+              self.tracefile + '.test'])
+        call([self.executable_name, '-a', self.tracefile + '.base', '-a',
+              self.tracefile + '.test', '-o', self.tracefile])
+        call([self.executable_name, '-r', self.tracefile + '.test',
+              '/usr/include/', '-o', self.tracefile])
+        call([self.html_generator_name, self.tracefile, '--output-directory',
+              self.output_path])
+
+
+class LizardTool(Tool):
+    def __init__(self, executable_name, minimum_version):
+        Tool.__init__(self, executable_name, minimum_version)
+
+    def get_version(self):
+        s = get_command_output([self.executable_name, '--version'])
+        return s[1].strip().split('.')
+
+
+class MakeTool(Tool):
+    def __init__(self, executable_name, minimum_version, build_path):
+        Tool.__init__(self, executable_name, minimum_version)
+        self.build_path = build_path
+
+    def get_version(self):
+        s = get_command_output([self.executable_name, '--version'])
+        print(s[0].split('\n')[0].split()[-1].split('.'))
+        return s[0].split('\n')[0].split()[-1].split('.')
+
+    def execute(self, args, working_path='.'):
+        call([self.executable_name] + args, cwd=working_path)
+
+
+class RPMBuildTool(Tool):
+    def __init__(self, executable_name, minimum_version):
+        Tool.__init__(self, executable_name, minimum_version)
+
+    def get_version(self):
+        s = get_command_output([self.executable_name, '--version'])
+        return s[0].strip().split()[-1].split('.')
+
+
+class RATSTool(Tool):
+    def __init__(self, executable_name, minimum_version, source_paths):
+        Tool.__init__(self, executable_name, minimum_version)
+        self.source_paths = source_paths
+
+    def get_version(self):
+        s = get_command_output([self.executable_name, '-h'])
+        return s[0].split('\n')[0].split()[1][1:].split('.')
+
+    def execute(self):
+        call([self.executable_name, '--resultsonly', '-w', '3'] +
+             self.source_paths)
+
+
+class ValgrindTool(Tool):
+    def __init__(self, executable_name, minimum_version):
+        Tool.__init__(self, executable_name, minimum_version)
+
+    def get_version(self):
+        s = get_command_output([self.executable_name, '--version'])
+        return s[0].strip().split('-')[-1].split('.')
+
+    def execute(self, application):
+        call([self.executable_name, application])
