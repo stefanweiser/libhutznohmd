4eb52c7c65395d117b4c6782c073e03c2bfae9f3
Stefan Weiser <stefan.weiser@bluewin.ch>
2014-08-18 16:45:47 +0200

    Added cmake code coverage modules.

---
+++ cmake/Coverage.cmake
@@ -0,0 +1,35 @@
+# This file is part of librestsrv.
+# Copyright (C) 2013-2014 Stefan Weiser
+#
+# The librestsrv project is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 3.0 of the License, or (at your
+# option) any later version.
+#
+# The librestsrv project is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+# License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with the librestsrv project; if not, see <http://www.gnu.org/licenses/>.
+
+
+
+FUNCTION(SETUP_TARGET_FOR_COVERAGE TARGET TEST EXCLUDE)
+    IF(LCOV_FOUND)
+        SET(OUTPUT "${TEST}_cov")
+
+        ADD_CUSTOM_TARGET(${TARGET}
+                          ${LCOV_LCOV_EXECUTABLE} -d . -z
+                          COMMAND ${TEST}
+                          COMMAND ${LCOV_LCOV_EXECUTABLE} -d . -c -o ${OUTPUT}.info
+                          COMMAND ${LCOV_LCOV_EXECUTABLE} -r ${OUTPUT}.info ${EXCLUDE} 'gmock/*' '/usr/*' -o ${OUTPUT}.info.cleaned
+                          COMMAND ${LCOV_GENHTML_EXECUTABLE} -o ${OUTPUT} ${OUTPUT}.info.cleaned
+                          COMMAND ${CMAKE_COMMAND} -E remove ${OUTPUT}.info ${OUTPUT}.info.cleaned
+                          WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
+    ELSE()
+        MESSAGE(WARNING "Target ${TARGET} not available, because lcov is missing.")
+    ENDIF()
+ENDFUNCTION()
+
