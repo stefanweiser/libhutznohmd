ae906f793bdbf1956d3016330105a7cc94b5ccde
Stefan Weiser <stefan.weiser@bluewin.ch>
2014-09-12 20:52:45 +0200

    Updated license to conform 80 characters.

--- src/http/request.hpp
+++ src/http/request.hpp
@@ -1,18 +1,19 @@
 /* This file is part of librestsrv.
  * Copyright (C) 2013-2014 Stefan Weiser
 
- * The librestsrv project is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 3.0 of the License, or (at your
- * option) any later version.
+ * The librestsrv project is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3.0 of the
+ * License, or (at your option) any later version.
 
- * The librestsrv project is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
- * License for more details.
+ * The librestsrv project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+ * General Public License for more details.
 
  * You should have received a copy of the GNU Lesser General Public License
- * along with the librestsrv project; if not, see <http://www.gnu.org/licenses/>.
+ * along with the librestsrv project; if not, see
+ * <http://www.gnu.org/licenses/>.
  */
 
 #ifndef LIBREST_HTTP_REQUEST_HPP
