b2d7a06b21ac00f88dd0178453fcc31c92cfa655
Stefan Weiser <stefan.weiser@bluewin.ch>
2014-10-03 21:59:51 +0200

    Added a lot of checks to detect compatibility with the host system.

--- cmake/CheckToolchain.cmake
+++ cmake/CheckToolchain.cmake
@@ -17,6 +17,10 @@
 ENABLE_LANGUAGE(CXX)
 
 INCLUDE(CheckIncludeFileCXX)
+INCLUDE(CheckCXXCompilerFlag)
+INCLUDE(CheckCXXSourceCompiles)
+INCLUDE(CheckPrototypeDefinition)
+INCLUDE(CheckStructHasMember)
 
 CHECK_INCLUDE_FILE_CXX(arpa/inet.h __HAS_ARPA_INET_H__)
 CHECK_INCLUDE_FILE_CXX(fcntl.h __HAS_FCNTL_H__)
@@ -36,8 +40,105 @@ IF(NOT __HAS_ARPA_INET_H__ OR
     MESSAGE(FATAL_ERROR "One of the required include files were not found.")
 ENDIF()
 
-INCLUDE(CheckCXXCompilerFlag)
-
 IF(("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang") OR ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU"))
     CHECK_CXX_COMPILER_FLAG("-std=c++11" __CXX11_AVAILABLE__)
 ENDIF()
+
+CHECK_STRUCT_HAS_MEMBER("struct linger" l_onoff "sys/socket.h" __HAS_LINGER_ONOFF__)
+CHECK_STRUCT_HAS_MEMBER("struct linger" l_linger "sys/socket.h" __HAS_LINGER_LINGER__)
+CHECK_STRUCT_HAS_MEMBER("struct pollfd" fd "sys/poll.h" __HAS_POLLFD_FD__)
+CHECK_STRUCT_HAS_MEMBER("struct pollfd" events "sys/poll.h" __HAS_POLLFD_EVENTS__)
+CHECK_STRUCT_HAS_MEMBER("struct pollfd" revents "sys/poll.h" __HAS_POLLFD_REVENTS__)
+CHECK_STRUCT_HAS_MEMBER("struct sockaddr_in" sin_family "netinet/in.h" __HAS_SOCKADDR_IN_FAMILY__)
+CHECK_STRUCT_HAS_MEMBER("struct sockaddr_in" sin_port "netinet/in.h" __HAS_SOCKADDR_IN_PORT__)
+CHECK_STRUCT_HAS_MEMBER("struct sockaddr_in" sin_addr "netinet/in.h" __HAS_SOCKADDR_IN_ADDR__)
+CHECK_STRUCT_HAS_MEMBER("struct in_addr" s_addr "netinet/in.h" __HAS_IN_ADDR_ADDR__)
+
+CHECK_PROTOTYPE_DEFINITION(accept
+                           "int accept(int __fd, __SOCKADDR_ARG __addr, socklen_t *__restrict __addr_len)"
+                           "0"
+                           "sys/socket.h"
+                           __HAS_ACCEPT__)
+CHECK_PROTOTYPE_DEFINITION(bind
+                           "int bind(int __fd, const struct sockaddr *__addr, socklen_t __addrlen)"
+                           "0"
+                           "sys/socket.h"
+                           __HAS_BIND__)
+CHECK_PROTOTYPE_DEFINITION(connect
+                           "int connect(int __fd, __CONST_SOCKADDR_ARG __addr, socklen_t __len)"
+                           "0"
+                           "sys/socket.h"
+                           __HAS_CONNECT__)
+CHECK_PROTOTYPE_DEFINITION(getsockopt
+                           "int getsockopt(int __fd, int __level, int __optname, void *__restrict __optval, socklen_t *__restrict __optlen)"
+                           "0"
+                           "sys/socket.h"
+                           __HAS_GETSOCKOPT__)
+CHECK_PROTOTYPE_DEFINITION(listen
+                           "int listen(int __fd, int __n)"
+                           "0"
+                           "sys/socket.h"
+                           __HAS_LISTEN__)
+CHECK_PROTOTYPE_DEFINITION(recv
+                           "ssize_t recv(int __fd, void *__buf, size_t __n, int __flags)"
+                           "0"
+                           "sys/socket.h"
+                           __HAS_RECV__)
+CHECK_PROTOTYPE_DEFINITION(send
+                           "ssize_t send(int __fd, __const void *__buf, size_t __n, int __flags)"
+                           "0"
+                           "sys/socket.h"
+                           __HAS_SEND__)
+CHECK_PROTOTYPE_DEFINITION(setsockopt
+                           "int setsockopt(int __fd, int __level, int __optname, __const void *__optval, socklen_t __optlen)"
+                           "NULL"
+                           "sys/socket.h"
+                           __HAS_SETSOCKOPT__)
+CHECK_PROTOTYPE_DEFINITION(shutdown
+                           "int shutdown(int __fd, int __how)"
+                           "0"
+                           "sys/socket.h"
+                           __HAS_SHUTDOWN__)
+CHECK_PROTOTYPE_DEFINITION(socket
+                           "int socket(int __domain, int __type, int __protocol)"
+                           "0"
+                           "sys/socket.h"
+                           __HAS_SOCKET__)
+
+CHECK_PROTOTYPE_DEFINITION(close
+                           "int close(int __fd)"
+                           "0"
+                           "unistd.h"
+                           __HAS_CLOSE__)
+CHECK_PROTOTYPE_DEFINITION(usleep
+                           "int usleep(__useconds_t __useconds)"
+                           "0"
+                           "unistd.h"
+                           __HAS_USLEEP__)
+
+CHECK_PROTOTYPE_DEFINITION(htons
+                           "uint16_t htons(uint16_t __hostshort)"
+                           "0"
+                           "arpa/inet.h"
+                           __HAS_HTONS__)
+CHECK_PROTOTYPE_DEFINITION(inet_aton
+                           "int inet_aton(__const char *__cp, struct in_addr *__inp)"
+                           "0"
+                           "arpa/inet.h"
+                           __HAS_INET_ATON__)
+CHECK_PROTOTYPE_DEFINITION(ntohs
+                           "uint16_t ntohs(uint16_t __netshort)"
+                           "0"
+                           "arpa/inet.h"
+                           __HAS_NTOHS__)
+CHECK_PROTOTYPE_DEFINITION(ntohl
+                           "uint32_t ntohl(uint32_t __netlong)"
+                           "0"
+                           "arpa/inet.h"
+                           __HAS_NTOHL__)
+
+CHECK_PROTOTYPE_DEFINITION(poll
+                           "int poll(struct pollfd *__fds, nfds_t __nfds, int __timeout)"
+                           "0"
+                           "sys/poll.h"
+                           __HAS_POLL__)
