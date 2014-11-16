be8bd38fd21c0d9ef3f42f9a6041f7f2dde05d27
Stefan Weiser <stefan.weiser@bluewin.ch>
2014-11-16 19:32:25 +0100

    Added new interaction concept.

--- src/doc/mainconcept.hpp
+++ src/doc/mainconcept.hpp
@@ -19,12 +19,57 @@
 #error "Do not include this pure documentation file."
 
 /*!
+
+@page interaction_concept Interaction concept
+
+To build a restful web service there are two things needed at the most
+abstracted level:
+
+-# An abstraction of the source and the destination of data (e.g. sockets).
+-# A multiplexing component, that helps to generate the correct response on any
+request.
+
+This library is solving this in separated components. There are interfaces for
+socket communication and multiplexing requests, but no code to connect those
+components. The user has to connect this by own code:
+
+@startuml{source_multiplexing_destination.png}
+skinparam component {
+    BackgroundColor<<user>> green
+    BackgroundColor<<libhutznohmd>> red
+}
+
+[control code] <<user>> as control_code
+[listener socket] <<libhutznohmd>> as listener_socket
+[request multiplexer] <<libhutznohmd>> as request_multiplexer
+[resource function] <<user>> as resource_function
+[resource manager] <<user>> as resource_manager
+
+control_code -down-> listener_socket : listens and accepts
+control_code -right-> request_multiplexer : provides connection_sockets
+request_multiplexer -right-> resource_function : uses to process requests
+resource_manager -down-> request_multiplexer : manages resources
+@enduml
+
+Therefore the library does not enforce its users to process the requests in a
+predefined way. This decision has some advantages and some disadvantages.
+
+Pro:
+- more flexible
+- modular concept
+- no multithreading in the library
+
+Contra:
+- more possibilities for errors
+
+Though it is recommended only to connect library components with each other,
+this is not enforced.
+
 @page concept_overview Concept Overview
 
 The library consists mainly of two packages. One with helper classes for a
 restful web service and one with a http server. Both are visible for the user
-and
-therefore can be used. This should enable the user to use only specific
+and therefore can be used. This should enable the user to use only specific
 components.
 
 @startuml{main_concept.png}
