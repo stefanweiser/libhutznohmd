321192b2a7221e31e7d8b4fbe9dfe3c97d795935
Stefan Weiser <stefan.weiser@bluewin.ch>
2015-01-23 19:44:37 +0100

    Improved etymology.

--- src/doc/etymology.hpp
+++ src/doc/etymology.hpp
@@ -27,12 +27,12 @@ populated during the medieval times, because silver and other ores were
 discovered there. During the 16th century the Ore Mountains were the heart of
 the mining industry in central europe.
 
-In the 17th century economical crisis especially the Thirty-Years-War disrupted
-the mining boom in the Ore Mountains. The population had to find other
+In the 17th century economical crisis and especially the Thirty-Years-War
+disrupted the mining boom in the Ore Mountains. The population had to find other
 occupations. The textile industry and the industry of producing wooden goods and
 toys arose out of this. Knipling became increasingly popular. Due to the harsh
 climate, to save firewood and because of companion, the women began to work
-together in one room. This was the birthplace of the Hutznstube.
+together in one room. This was the birth of the Hutznstube.
 
 Slowly the Hutzenstube got filled with tradition. The men often joined their
 wives after work. They started carving, smoked pipe, talked and sang together.
@@ -41,12 +41,12 @@ Nowadays the Hutzenabend gets more and more an event including dinner and music.
 
 @section sec_etymology Etymology
 
-The easy word Abend (Ohmd or Obnd in the dialect of the western Ore Mountains)
-is the german word for the evening.
+The word Abend (Ohmd or Obnd in the dialect of the western Ore Mountains) is the
+german word for the evening.
 
 The word hutzen (a german weak verb) is a bit complicated, because it is not
-used as verb anymore and nowadays all existing sources conflict each other. In
-the dialect of the Ore Mountains (as in Vogtland and Fraconia) it was used to
-say to visit people in a strolling manner.
+used as verb anymore and nowadays all existing sources seem to conflict each
+other. In the dialect of the Ore Mountains (as in Vogtland and Fraconia) it was
+used to say: "to meet people by strolling to them".
 
 */
