50f9f2fc077926b8eb9bb51863aa43d1a45c93a4
Stefan Weiser <stefan.weiser@bluewin.ch>
2015-01-23 19:44:42 +0100

    Moved some documentation.

--- src/hutzn.hpp
+++ src/hutzn.hpp
@@ -34,6 +34,115 @@
 namespace hutzn
 {
 
+/*!
+
+@page deployment Deployment
+
+@section sec_quick_start Quick Start
+
+All deployment steps are done by the @c make script. A help could be printed out
+by:
+
+@code
+$ ./make --help
+@endcode
+
+Normally making @c all in both targets (@c --debug and @c --release) and making
+@c --coverage is of interest.
+
+@section sec_deploying Deploying
+
+Deploying a version is done by incrementing the version and making a package:
+
+@code
+./make --release --minimal package
+@endcode
+
+These packages are ought to be released. Create a tag on the git repository
+afterwards.
+
+
+
+@page development Development
+
+@todo [DOC] Fill concepts.
+
+@section sec_principles Development Principles
+
+To aim a homogenous code base, it is indispensable to have some principles on
+how
+to work.
+
+I spent much time on porting code of software dinosaurs and therefor
+reengineering the meaning of it. Often I have chosen to rewrite it component by
+component, because the original code is somewhat bloated. Sometimes I got tired
+of fixing or removing the one-thousand-and-first idea of slightly improving the
+performance at the expense of a massively higher risk of raising maintenance
+costs, where another improvement would do the same, but more reliable.
+
+However, there are some principles, that are essential:
+
+- @b KISS: The code has to be as simple as possible. Any non-intuitive solution
+for a problem should be documented.
+- @b SOLID: This should keep the architecture extensible.
+- @b AAA: The tests should be arranged in that pattern, but there are good
+reasons that not all tests may fulfill this. A triple-A test is better than one
+that is arranged in another way.
+- @b YAGNI: Keep this in mind. If some code is not needed anymore, it shall be
+removed.
+
+In any cases it will be difficult where to draw the line. Don't be religious
+with them, but keep them in mind and improve the code.
+
+@section sec_who Who can contribute?
+
+Anyone who is passionated in developing software and is sharing the principles
+of this project is welcome to contribute to it. Please use a pull request on
+GitHub.
+
+@section sec_tools Tools
+
+Generally:
+- @b bash >= 4.2
+- @b git >= 1.7.9
+- @b cmake >= 2.8
+
+For building:
+- @b g++ >= 4.8
+- @b clang >= 3.4
+- @b cppcheck >= 1.60
+- @b clang-format >= 3.5
+
+For developing:
+- @b qtcreator >= 2.8
+
+For documentation:
+- @b doxygen >= 1.7.6
+- @b dot >= 2.26
+- @b java >= 1.6
+
+For coverage:
+- @b gcov >= 4.8
+- @b lcov >= 1.9
+
+For valgrind:
+- @b valgrind >= 3.7
+
+For metrics:
+- @b lizard >= 1.8.4 (see https://pypi.python.org/pypi/lizard)
+
+For packaging:
+- @b tar >= 1.26
+
+@section sec_concepts Concepts
+
+- @ref interaction_concept
+- @ref roadmap
+- @ref header_fields
+- @ref http_parser
+
+*/
+
 //! Every HTTP request has a specific method. There are two properties to a
 //! subset of methods.
 //! 1. Idempotence: You can apply the identical request n times to the same URI
