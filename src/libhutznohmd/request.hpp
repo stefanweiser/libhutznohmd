e0d2983dadfc32dddef74b061171ef6ec8431730
Stefan Weiser <stefan.weiser@bluewin.ch>
2015-01-23 19:45:00 +0100

    Moved header field documentation to request header.

--- src/libhutznohmd/request.hpp
+++ src/libhutznohmd/request.hpp
@@ -29,21 +29,20 @@ namespace hutzn
 @page requests Requests
 
 Parsing a request is done by an internal HTTP parser. It offers the server an
-optimized view on the request. On the other hand the server has to generate a
-response. The following activity diagram is a simplification of the control
-flow during request handling:
+optimized view on the request. The server also has to generate a response. The
+following activity diagram is a simplification of the control flow during
+request handling:
 
 @startuml{request_activity.svg}
 
 start
-
 :read request;
-if (request ok) then (yes)
-    if (handler exists) then (yes)
+if (request ok?) then (yes)
+    if (handler exists?) then (yes)
         :call handler;
         :write response;
     else (no)
-        if (error handler exists) then (yes)
+        if (error handler exists?) then (yes)
             :call error handler;
             :write response;
         else (no)
@@ -53,7 +52,6 @@ if (request ok) then (yes)
 else (no)
     :write bad request error response;
 endif
-
 stop
 
 @enduml
@@ -76,6 +74,338 @@ expect? Informations on the content.
 -# @subpage page_cookies
 -# @subpage page_byte_serving
 -# @subpage page_conditional
+
+
+
+@page page_basic_http_support Basic HTTP support
+
+@section sec_basic_common Common
+
+@subsection sub_connection Connection
+
+Determines, what to do with the connection after the request is finished. Two
+values
+are possible: @c close and @c keep-alive.
+
+In case of @c close the connection will be closed after the response is sent and
+in case of
+@c keep-alive the connection will be kept until the client closes the connection
+or a predefined
+time duration elapsed without activity on the connection. This time duration can
+be configured.
+@todo How it can be configured.
+
+@a Example:
+@code
+Connection: keep-alive
+@endcode
+
+@a Default:
+
+till HTTP/1.0:
+@code
+Connection: close
+@endcode
+
+since HTTP/1.1:
+@code
+Connection: keep-alive
+@endcode
+
+@subsection sub_content_length Content-Length
+
+The header field of the @c Content-Length must be added by the client in
+requests, if the request
+carries any payload data. The server will add this header field, if the payload
+data of the
+response is not empty. It must then contain the size of the payload in bytes.
+Thus it must be an
+unsigned integer. The size is limited to \f$2^{31}-1\f$. In case of an overflow
+the request gets
+rejected.
+
+@a Example:
+@code
+Content-Length: 123
+@endcode
+
+@a Default:
+@code
+Content-Length: 0
+@endcode
+
+@subsection sub_content_md5 Content-MD5
+
+This header field is optional and carries a MD5 hash sum. This hash sum is used
+to verify the
+content. If the header field is missing, the content is not verified with MD5.
+Thus there is no
+default value.
+
+@note This is no security feature, because of 2 reasons:
+-# Collisions for MD5 could be found within reasonable time by recent hardware.
+-# An attacker that is able to modify the header field or the content is
+everytime also able to
+modify both.
+
+@a Example:
+@code
+Content-MD5: Q2hlY2sgSW50ZWdyaXR5IQ==
+@endcode
+
+@subsection sub_content_type Content-Type
+
+A content type header defines how the application should interpret the content.
+It consists of one
+MIME type.
+
+@a Example:
+@code
+Content-Type: application/xml
+@endcode
+
+@a Default:
+@code
+Content-Type: text/plain
+@endcode
+
+@subsection sub_content_date Date
+
+This header contains the timestamp, when the message was generated.
+
+@todo Explain, when the date is used and how it can be configured.
+
+@a Example:
+@code
+Date: Wed, 13 May 2014 22:10:48 GMT
+@endcode
+
+@section sec_basic_request Request-Specific
+
+@subsection sub_accept Accept
+
+<td>TBD</td>
+<td><pre>Accept: text/plain</pre></td>
+
+@subsection sub_expect Expect
+
+<td>TBD</td>
+<td><pre>Expect: 100-continue</pre></td>
+
+@subsection sub_from From
+
+Contains an e-mail address of a human, that controls the requesting user agent.
+
+@a Example:
+@code
+From: user@example.com
+@endcode
+
+@subsection sub_host Host
+
+Refers to the requested host on the server in case of multiple domains hosted on
+the server.
+Therefore it is part of the request URI. If the header is present, the content
+may overwrite the
+content of the request URI.
+
+@a Example:
+@code
+Host: example.com
+@endcode
+
+@subsection sub_referer Referer
+
+<td>TBD</td>
+<td><pre>Referer: http://www.example.com/</pre></td>
+
+@subsection sub_user_agent User-Agent
+
+<td>TBD</td>
+<td><pre>User-Agent: Googlebot/2.1 (+http://www.google.com/bot.html)</pre></td>
+
+@section sec_basic_response Response-Specific
+
+@subsection sub_allow Allow
+
+<td>TBD</td>
+<td><pre>Allow: GET, HEAD, PUT</pre></td>
+
+@subsection sub_content_location Content-Location
+
+<td>TBD</td>
+<td><pre>Content-Location: /example</pre></td>
+
+@subsection sub_etag ETag
+
+<td>TBD</td>
+<td><pre>ETag: "0123456789abcdef"</pre></td>
+
+@subsection sub_location Location
+
+<td>TBD</td>
+<td><pre>Location: http://example.com/example</pre></td>
+
+@subsection sub_retry_after Retry-After
+
+<td>TBD</td>
+<td><pre>Retry-After: 120</pre></td>
+
+@subsection sub_server Server
+
+<td>TBD</td>
+<td><pre>Server: librest/x.y.z</pre></td>
+
+
+
+@page page_encoding Encoding
+
+@section sec_encoding_common Common
+
+@subsection sub_content_encoding Content-Encoding
+
+<td>TBD</td>
+<td><pre>Content-Encoding: gzip</pre></td>
+
+@subsection sub_content_language Content-Language
+
+<td>TBD</td>
+<td><pre>Content-Language: en-US</pre></td>
+
+@section sec_encoding_request Request-Specific
+
+@subsection sub_accept_charset Accept-Charset
+
+<td>TBD</td>
+<td><pre>Accept-Charset: utf-8</pre></td>
+
+@subsection sub_accept_encoding Accept-Encoding
+
+<td>TBD</td>
+<td><pre>Accept-Encoding: gzip</pre></td>
+
+@subsection sub_accept_language Accept-Language
+
+<td>TBD</td>
+<td><pre>Accept-Language: en-US</pre></td>
+
+
+
+@page page_auth Authentification / Authorization
+
+@section sec_auth_request Request-Specific
+
+@subsection sub_authorization Authorization
+
+<td>TBD</td>
+<td><pre>Authorization: Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==</pre></td>
+
+@section sec_auth_response Response-Specific
+
+@subsection sub_www_authenticate WWW-Authenticate
+
+<td>TBD</td>
+<td><pre>WWW-Authenticate: Basic</pre></td>
+
+
+
+@page page_caching Caching
+
+@section sec_caching_common Common
+
+@subsection sub_cache_control Cache-Control
+
+<td>TBD</td>
+<td><pre>Cache-Control: no-cache</pre></td>
+
+@section sec_caching_response Response-Specific
+
+@subsection sub_expires Expires
+
+<td>TBD</td>
+<td><pre>Expires: Wed, 13 May 2014 22:10:48 GMT</pre></td>
+
+@subsection sub_last_modified Last-Modified
+
+<td>TBD</td>
+<td><pre>Last-Modified: Wed, 13 May 2014 22:10:48 GMT</pre></td>
+
+@subsection sub_content_pragma Pragma
+
+<td>TBD</td>
+<td><pre>Pragma: no-cache</pre></td>
+
+
+
+@page page_cookies Cookies
+
+@section sec_cookies_request Request-Specific
+
+@subsection sub_cookie Cookie
+
+<td>TBD</td>
+<td><pre>Cookie: id=123</pre></td>
+
+@section sec_cookies_response Response-Specific
+
+@subsection sub_set_cookie Set-Cookie
+
+<td>TBD</td>
+<td><pre>Set-Cookie: id=123</pre></td>
+
+
+
+@page page_byte_serving Byte-Serving
+
+@section sec_byte_serving_request Request-Specific
+
+@subsection sub_range Range
+
+<td>TBD</td>
+<td><pre>Range: bytes=0-2048</pre></td>
+
+@section sec_byte_serving_response Response-Specific
+
+@subsection sub_accept_ranges Accept-Ranges
+
+<td>TBD</td>
+<td><pre>Accept-Ranges: bytes</pre></td>
+
+@subsection sub_content_range Content-Range
+
+<td>TBD</td>
+<td><pre>Content-Range: 0-2048/4096</pre></td>
+
+
+
+@page page_conditional Conditional Requests
+
+@section sec_conditional_request Request-Specific
+
+@subsection sub_if_match If-Match
+
+<td>TBD</td>
+<td><pre>If-Match: "0123456789abcdef"</pre></td>
+
+@subsection sub_if_modified_since If-Modified-Since
+
+<td>TBD</td>
+<td><pre>If-Modified-Since: Wed, 13 May 2014 22:10:48 GMT</pre></td>
+
+@subsection sub_if_none_match If-None-Match
+
+<td>TBD</td>
+<td><pre>If-None-Match: "0123456789abcdef"</pre></td>
+
+@subsection sub_if_range If-Range
+
+<td>TBD</td>
+<td><pre>If-Range: "0123456789abcdef"</pre></td>
+
+@subsection sub_if_unmodified_since If-Unmodified-Since
+
+<td>TBD</td>
+<td><pre>If-Unmodified-Since: Wed, 13 May 2014 22:10:48 GMT</pre></td>
 
 */
 
