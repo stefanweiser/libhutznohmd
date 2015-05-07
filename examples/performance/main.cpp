a34b29ad6ec56b780be8149e67a193cca1ac2bfd
Stefan Weiser <stefan.weiser@bluewin.ch>
2015-05-07 20:30:31 +0200

    Removed namespace hutzn from examples.

--- examples/performance/main.cpp
+++ examples/performance/main.cpp
@@ -26,15 +26,10 @@
 #include <http/parser/utility/trie.hpp>
 #include <http/parser/utility/uri.hpp>
 
-namespace hutzn
-{
-
-namespace http
-{
-
-namespace
-{
-
+template <typename value_type>
+using trie = hutzn::http::trie<value_type>;
+using header_type = hutzn::http::header_type;
+using anonymous_int_function = hutzn::http::anonymous_int_function;
 using string_index_pair = std::pair<std::string, size_t>;
 
 int32_t get_char(void* handle)
@@ -55,8 +50,6 @@ int32_t peek_char(void* handle)
     return -1;
 }
 
-} // namespace
-
 void test_trie_parse(const std::string& token)
 {
     const std::vector<trie<header_type>::value_info> values = {
@@ -69,10 +62,10 @@ void test_trie_parse(const std::string& token)
     std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
     for (size_t i = 0; i < 1000000; i++) {
         string_index_pair p(token, 0);
-        lexer l(anonymous_int_function(&get_char, &p),
-                anonymous_int_function(&peek_char, &p));
+        hutzn::http::lexer l(anonymous_int_function(&get_char, &p),
+                             anonymous_int_function(&peek_char, &p));
         int32_t character = l.get();
-        push_back_string<4> fail_safe_result;
+        hutzn::http::push_back_string<4> fail_safe_result;
         test_trie.parse(character, fail_safe_result, l);
     }
     std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
@@ -88,15 +81,17 @@ void test_req_parser(const std::string& request)
         // This initializes all static variables, that else would sophisticate
         // the results.
         string_index_pair p(request, 0);
-        request_parser parser(anonymous_int_function(&get_char, &p),
-                              anonymous_int_function(&peek_char, &p));
+        hutzn::http::request_parser parser(
+            anonymous_int_function(&get_char, &p),
+            anonymous_int_function(&peek_char, &p));
         parser.parse();
     }
     std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
     for (size_t i = 0; i < 1000; i++) {
         string_index_pair p(request, 0);
-        request_parser parser(anonymous_int_function(&get_char, &p),
-                              anonymous_int_function(&peek_char, &p));
+        hutzn::http::request_parser parser(
+            anonymous_int_function(&get_char, &p),
+            anonymous_int_function(&peek_char, &p));
         parser.parse();
     }
     std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
@@ -112,8 +107,8 @@ void test_http_date_parser(const std::string& date_string)
         // This initializes all static variables, that else would sophisticate
         // the results.
         string_index_pair p(date_string, 0);
-        lexer l(anonymous_int_function(&get_char, &p),
-                anonymous_int_function(&peek_char, &p));
+        hutzn::http::lexer l(anonymous_int_function(&get_char, &p),
+                             anonymous_int_function(&peek_char, &p));
         int32_t result = l.get();
         parse_timestamp(result, l);
     }
@@ -121,8 +116,8 @@ void test_http_date_parser(const std::string& date_string)
     std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
     for (size_t i = 0; i < 1000; i++) {
         string_index_pair p(date_string, 0);
-        lexer l(anonymous_int_function(&get_char, &p),
-                anonymous_int_function(&peek_char, &p));
+        hutzn::http::lexer l(anonymous_int_function(&get_char, &p),
+                             anonymous_int_function(&peek_char, &p));
         int32_t result = l.get();
         parse_timestamp(result, l);
     }
@@ -139,8 +134,8 @@ void test_uri_parser(const std::string& uri)
         // This initializes all static variables, that else would sophisticate
         // the results.
         string_index_pair p(uri, 0);
-        lexer l(anonymous_int_function(&get_char, &p),
-                anonymous_int_function(&peek_char, &p));
+        hutzn::http::lexer l(anonymous_int_function(&get_char, &p),
+                             anonymous_int_function(&peek_char, &p));
         int32_t result = l.get();
         hutzn::http::uri u;
         u.parse(l, result, false);
@@ -149,8 +144,8 @@ void test_uri_parser(const std::string& uri)
     std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
     for (size_t i = 0; i < 1000000; i++) {
         string_index_pair p(uri, 0);
-        lexer l(anonymous_int_function(&get_char, &p),
-                anonymous_int_function(&peek_char, &p));
+        hutzn::http::lexer l(anonymous_int_function(&get_char, &p),
+                             anonymous_int_function(&peek_char, &p));
         int32_t result = l.get();
         hutzn::http::uri u;
         u.parse(l, result, false);
@@ -162,18 +157,14 @@ void test_uri_parser(const std::string& uri)
               << " ns for uri: " << uri << std::endl;
 }
 
-} // namespace http
-
-} // namespace hutzn
-
 int main()
 {
     std::cout << "example_performance" << std::endl;
 
-    hutzn::http::test_req_parser("GET / HTTP/1.1\r\n\r\n");
-    hutzn::http::test_req_parser(
+    test_req_parser("GET / HTTP/1.1\r\n\r\n");
+    test_req_parser(
         "GET /what/a/long/url HTTP/1.1\r\nContent-Length: 0\r\n\r\n");
-    hutzn::http::test_req_parser(
+    test_req_parser(
         "GET /what/a/long/url HTTP/1.1\r\nContent-Length: 0\r\n"
         "Content-Length: 0\r\nContent-Length: 0\r\nContent-Length: 0\r\n"
         "Content-Length: 0\r\nContent-Length: 0\r\nContent-Length: 0\r\n"
@@ -183,20 +174,20 @@ int main()
         "Content-Length: 0\r\nContent-Length: 0\r\nContent-Length: 0\r\n"
         "Content-Length: 0\r\n\r\n");
 
-    hutzn::http::test_http_date_parser("Sun, 06 Nov 1994 08:49:37 GMT");
-    hutzn::http::test_http_date_parser("Sunday, 06-Nov-94 08:49:37 GMT");
-    hutzn::http::test_http_date_parser("Sun Nov  6 08:49:37 1994");
+    test_http_date_parser("Sun, 06 Nov 1994 08:49:37 GMT");
+    test_http_date_parser("Sunday, 06-Nov-94 08:49:37 GMT");
+    test_http_date_parser("Sun Nov  6 08:49:37 1994");
 
-    hutzn::http::test_trie_parse("");
-    hutzn::http::test_trie_parse("content-length");
-    hutzn::http::test_trie_parse("content-type");
-    hutzn::http::test_trie_parse("date");
+    test_trie_parse("");
+    test_trie_parse("content-length");
+    test_trie_parse("content-type");
+    test_trie_parse("date");
 
-    hutzn::http::test_uri_parser("/");
-    hutzn::http::test_uri_parser("http://localhost/");
-    hutzn::http::test_uri_parser("http://localhost:80/");
-    hutzn::http::test_uri_parser("http://user:pw@localhost:80/");
-    hutzn::http::test_uri_parser("http://user:pw@localhost:80/?a=b#anchor");
+    test_uri_parser("/");
+    test_uri_parser("http://localhost/");
+    test_uri_parser("http://localhost:80/");
+    test_uri_parser("http://user:pw@localhost:80/");
+    test_uri_parser("http://user:pw@localhost:80/?a=b#anchor");
 
     return 0;
 }
