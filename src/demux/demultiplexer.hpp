9e6cc3b448593b913ae4a3b8c6063d1eb135b836
Stefan Weiser <stefan.weiser@bluewin.ch>
2015-05-07 20:30:37 +0200

    Documented demultiplexer.

--- src/demux/demultiplexer.hpp
+++ src/demux/demultiplexer.hpp
@@ -32,6 +32,14 @@
 namespace hutzn
 {
 
+//! @brief Implements logic to determine the right request handler for a given
+//! request.
+//!
+//! Handles a request handler and a mime type and subtype storage. Implements
+//! all necessary interfaces to expose access to mime types, request handler
+//! management and the request handler determination algorithm. This class is
+//! designed to be thread safe. It does not call any callbacks into the user's
+//! domain.
 class demultiplexer : public demux_interface, public demux_disconnect_interface
 {
 public:
@@ -39,18 +47,12 @@ public:
 
     request_handler_callback determine_request_handler(
         const request_interface& request) override;
-
     handler_pointer connect(const request_handler_id& id,
                             const request_handler_callback& fn) override;
-
     bool disconnect(const request_handler_id& id) override;
-
     mime_type register_mime_type(const std::string& type) override;
-
     mime_subtype register_mime_subtype(const std::string& subtype) override;
-
     bool unregister_mime_type(const mime_type& type) override;
-
     bool unregister_mime_subtype(const mime_subtype& subtype) override;
 
 private:
@@ -59,12 +61,22 @@ private:
     using resource_method_map = std::map<http_verb, resource_mime_content_map>;
     using resource_map = std::map<std::string, resource_method_map>;
 
+    //! @brief Determines, whether a mime type is valid or not.
+    //!
+    //! Checks whether a type and subtype is consequently none or not. No part
+    //! must be invalid and both parts must be registered, when not none.
     bool is_mime_valid(const mime& t) const;
 
+    //! Guards the access to the resource map from mutual access.
     std::mutex resource_callbacks_mutex_;
+
+    //! Stores all request handler callbacks.
     resource_map resource_callbacks_;
 
+    //! Guards the request parser data from mutual access.
     std::mutex request_parser_data_mutex_;
+
+    //! Stores registered mime types.
     parser_data request_parser_data_;
 };
 
