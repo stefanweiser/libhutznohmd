ce37eafbdadc69d920ff830ba9913355f6f41e6e
Stefan Weiser <stefan.weiser@bluewin.ch>
2015-06-05 21:45:41 +0200

    Implemented request_handler_holder.

---
+++ src/demux/request_handler_holder.cpp
@@ -0,0 +1,47 @@
+/* This file is part of libhutznohmd.
+ * Copyright (C) 2013-2015 Stefan Weiser
+
+ * The libhutznohmd project is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3.0 of the
+ * License, or (at your option) any later version.
+
+ * The libhutznohmd project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+ * General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the libhutznohmd project; if not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#include "request_handler_holder.hpp"
+
+namespace hutzn
+{
+
+request_handler_holder::request_handler_holder(
+    usage_interface& demuxer, const request_handler_id& id,
+    const request_handler_callback& callback)
+    : demuxer_(demuxer)
+    , id_(id)
+    , callback_(callback)
+{
+}
+
+request_handler_holder::~request_handler_holder(void) noexcept(true)
+{
+    demuxer_.set_unused(id_);
+}
+
+http_status_code request_handler_holder::call(const request_interface& request,
+                                              response_interface& response)
+{
+    demuxer_.set_used(id_);
+    const http_status_code result = callback_(request, response);
+    demuxer_.set_unused(id_);
+    return result;
+}
+
+} // namespace hutzn
---
+++ src/demux/request_handler_holder.hpp
@@ -0,0 +1,51 @@
+/* This file is part of libhutznohmd.
+ * Copyright (C) 2013-2015 Stefan Weiser
+
+ * The libhutznohmd project is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3.0 of the
+ * License, or (at your option) any later version.
+
+ * The libhutznohmd project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+ * General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the libhutznohmd project; if not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LIBHUTZNOHMD_DEMUX_REQUEST_HANDLER_HOLDER_HPP
+#define LIBHUTZNOHMD_DEMUX_REQUEST_HANDLER_HOLDER_HPP
+
+#include <memory>
+
+#include <libhutznohmd/demux.hpp>
+
+#include <demux/usage_interface.hpp>
+
+namespace hutzn
+{
+
+class request_handler_holder : public request_handler_holder_interface
+{
+public:
+    explicit request_handler_holder(usage_interface& demuxer,
+                                    const request_handler_id& id,
+                                    const request_handler_callback& callback);
+
+    ~request_handler_holder(void) noexcept(true) override;
+
+    http_status_code call(const request_interface& request,
+                          response_interface& response) override;
+
+private:
+    usage_interface& demuxer_;
+    request_handler_id id_;
+    request_handler_callback callback_;
+};
+
+} // namespace hutzn
+
+#endif // LIBHUTZNOHMD_DEMUX_REQUEST_HANDLER_HOLDER_HPP
