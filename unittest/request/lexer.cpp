6d76abd78e6a82050569aa4e479183119b460ab6
Stefan Weiser <stefan.weiser@bluewin.ch>
2015-08-15 21:31:02 +0200

    Added more lexer tests.

--- unittest/request/lexer.cpp
+++ unittest/request/lexer.cpp
@@ -28,7 +28,53 @@ using namespace testing;
 namespace hutzn
 {
 
-TEST(lexer, construction)
+class lexer_test : public ::testing::Test
+{
+public:
+    void SetUp(void) override
+    {
+    }
+
+    void TearDown(void) override
+    {
+    }
+
+protected:
+    void check(const std::string& chunk, const std::string& result)
+    {
+        connection_mock_pointer c =
+            std::make_shared<connection_interface_mock>();
+        EXPECT_CALL(*c, receive(_, _))
+            .Times(2)
+            .WillOnce(Invoke([&chunk](buffer& b, const size_t& m) {
+                EXPECT_LE(chunk.size(), m);
+                b.insert(b.begin(), chunk.begin(), chunk.end());
+                return true;
+            }))
+            .WillOnce(Return(false));
+
+        lexer l(c);
+        for (size_t i = 0; i < result.size(); i++) {
+            EXPECT_EQ(static_cast<uint8_t>(result[i]), l.get());
+        }
+        EXPECT_EQ(-1, l.get());
+
+        for (size_t i = 0; i < result.size(); i++) {
+            const char_t* ch = l.data(i);
+            ASSERT_NE(nullptr, ch);
+            EXPECT_EQ(result[i], *ch);
+        }
+        EXPECT_EQ(nullptr, l.data(result.size()));
+
+        const char_t* data = l.data(0);
+        ASSERT_NE(nullptr, data);
+        for (size_t i = 0; i < result.size(); i++) {
+            EXPECT_EQ(static_cast<uint8_t>(result[i]), data[i]);
+        }
+    }
+};
+
+TEST_F(lexer_test, construction)
 {
     connection_mock_pointer c = std::make_shared<connection_interface_mock>();
     EXPECT_CALL(*c, receive(_, _)).Times(1).WillOnce(Return(false));
@@ -36,6 +82,61 @@ TEST(lexer, construction)
     lexer l(c);
     EXPECT_EQ(nullptr, l.data(0));
     EXPECT_EQ(-1, l.get());
+}
+
+TEST_F(lexer_test, call_receive_successfully_once)
+{
+    std::string chunk = "GET / HTTP/1.1\n\n";
+    check(chunk, chunk);
+}
+
+TEST_F(lexer_test, several_lws_tokens)
+{
+    std::string chunk = "a\n b\r\n c\r d\n\te\r\n\tf\r\tg";
+    std::string result = "a b c d e f g";
+    check(chunk, result);
+}
+
+TEST_F(lexer_test, newline_token_1)
+{
+    std::string chunk = "a\nb\r\nc\rd\n\n";
+    std::string result = "a\nb\nc\nd\n\n";
+    check(chunk, result);
+}
+
+TEST_F(lexer_test, newline_token_2)
+{
+    std::string chunk = "a\r\n\r\nb";
+    std::string result = "a\n\n";
+    check(chunk, result);
+}
+
+TEST_F(lexer_test, newline_token_3)
+{
+    std::string chunk = "a\r\rb";
+    std::string result = "a\n\n";
+    check(chunk, result);
+}
+
+TEST_F(lexer_test, newline_token_4)
+{
+    std::string chunk = "a\n\rb";
+    std::string result = "a\n\n";
+    check(chunk, result);
+}
+
+TEST_F(lexer_test, newline_token_5)
+{
+    std::string chunk = "a\n\r\nb";
+    std::string result = "a\n\n";
+    check(chunk, result);
+}
+
+TEST_F(lexer_test, newline_token_6)
+{
+    std::string chunk = "a\r\n\rb";
+    std::string result = "a\n\n";
+    check(chunk, result);
 }
 
 } // namespace hutzn
