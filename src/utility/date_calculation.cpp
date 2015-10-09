9ccf566d596ae64dcea93d85b4809c7084c763c1
Stefan Weiser <stefan.weiser@bluewin.ch>
2015-10-09 20:19:18 +0200

    Improve date_calculation's type usage and control flow.

--- src/utility/date_calculation.cpp
+++ src/utility/date_calculation.cpp
@@ -16,6 +16,8 @@
  * <http://www.gnu.org/licenses/>.
  */
 
+#include <array>
+
 #include <utility/common.hpp>
 
 #include "date_calculation.hpp"
@@ -26,76 +28,56 @@ namespace hutzn
 namespace
 {
 
-static const time_t years_between_leapyears = 4;
-static const time_t epoch_start_year = 1970;
-static const time_t usual_days_per_year = 365;
-static const time_t seconds_per_day = 86400;
+static const uint8_t years_between_leapyears = 4;
+static const uint8_t last_month = 12;
 
-time_t day_of_the_year(const time_t day, const time_t month, const time_t year)
+// Due to the leap year rule, that only century turns divisible by 400 are
+// leapyears, this algorithm is valid between 1970 and 2099.
+static const uint32_t epoch_start_year = 1970;
+static const uint32_t last_valid_year = 2099;
+
+//! Returns the 0-based day of the year, which means 0 for 1 January and 365 for
+//! 31 December of a leapyear.
+uint16_t day_of_the_year(const uint8_t day, const uint8_t month,
+                         const uint32_t year)
 {
-    time_t result = day;
-    static const int32_t base_days = 306;
-    if (month < 3) {
-        result += (base_days * month - 301) / 10;
-    } else {
-        result += (base_days * month - 913) / 10;
-        static const int32_t normal_rest_days = 60;
-        if ((year % years_between_leapyears) == 0) {
-            result += normal_rest_days;
-        } else {
-            result += normal_rest_days - 1;
-        }
+    static const std::array<uint16_t, last_month> days_in_year_per_month = {
+        {0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334}};
+    uint16_t result =
+        static_cast<uint16_t>(days_in_year_per_month[month - 1] + (day - 1));
+    if ((year % years_between_leapyears) == 0) {
+        result++;
     }
     return result;
 }
 
-bool is_valid_day(const time_t day, const time_t month, const time_t year)
+bool is_valid_epoch_date(const uint8_t day, const uint8_t month,
+                         const uint32_t year)
 {
-    static const time_t long_month = 31;
-    static const time_t normal_month = 30;
-    static const time_t short_month = 28;
-    static const time_t short_month_in_leapyear = 29;
+    static const uint8_t february = 2;
+    static const uint8_t first_month = 1;
 
-    if (day < 1) {
-        return false;
-    }
-
-    static const time_t juli = 7;
-    static const time_t even_month_modulo = 2;
-    if (month > juli) {
-        if ((month % even_month_modulo) == 0) {
-            return day <= long_month;
-        } else {
-            return day <= normal_month;
-        }
+    // A date is valid, when all of the following conditions are met:
+    // - the year is greater than or equal to the start year of epoch time
+    // - the month must be between 1 and 12
+    // - the day is between 1 and the correct days_per_month value, which
+    //   depends on month and year
+    bool result;
+    if ((!check_range<uint32_t, epoch_start_year, last_valid_year>(year)) ||
+        (!check_range<uint8_t, first_month, last_month>(month)) || (day < 1)) {
+        result = false;
     } else {
-        static const time_t february = 2;
-        if (month == february) {
-            if ((year % years_between_leapyears) == 0) {
-                return day <= short_month_in_leapyear;
-            } else {
-                return day <= short_month;
-            }
-        } else if ((month % even_month_modulo) == 0) {
-            return day <= normal_month;
+        // Note, that the month is 1-based and the index of this array is
+        // 0-based.
+        static const std::array<uint8_t, last_month> days_per_month = {
+            {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}};
+        if ((month == february) && ((year % years_between_leapyears) == 0)) {
+            result = day <= (days_per_month[month - 1] + 1);
         } else {
-            return day <= long_month;
+            result = day <= days_per_month[month - 1];
         }
     }
-}
-
-bool is_valid_epoch_date(const time_t day, const time_t month,
-                         const time_t year)
-{
-    static const time_t first_month = 1;
-    static const time_t last_month = 12;
-
-    if ((year < epoch_start_year) ||
-        (!check_range<time_t, first_month, last_month>(month)) ||
-        (!is_valid_day(day, month, year))) {
-        return false;
-    }
-    return true;
+    return result;
 }
 
 } // namespace
@@ -104,19 +86,33 @@ epoch_time_t seconds_since_epoch(const uint32_t second_of_day,
                                  const uint8_t day, const uint8_t month,
                                  const uint32_t year)
 {
-    if (!is_valid_epoch_date(day, month, year)) {
-        return -1;
+    static const uint16_t usual_days_per_year = 365;
+    static const uint32_t seconds_per_day = 86400;
+
+    epoch_time_t result;
+    if ((!is_valid_epoch_date(day, month, year)) ||
+        (second_of_day >= seconds_per_day)) {
+        result = -1;
+    } else {
+        const uint32_t year_since_epoch = year - epoch_start_year;
+
+        // Only full years count here. Since 1972 was the first leapyear in
+        // epoch, the leapyear calculation has to start with 1969 or the result
+        // of 1973 would mistakenly be 0.
+        const uint32_t days_due_to_leapyears =
+            (year_since_epoch + 1) / years_between_leapyears;
+
+        const uint32_t usual_days_since_epoch =
+            year_since_epoch * usual_days_per_year;
+        const uint32_t days_since_epoch =
+            usual_days_since_epoch + days_due_to_leapyears;
+
+        const uint32_t day_of_year = day_of_the_year(day, month, year);
+
+        result = (days_since_epoch * seconds_per_day) +
+                 (day_of_year * seconds_per_day) + second_of_day;
     }
-
-    const time_t second_of_year =
-        second_of_day +
-        ((day_of_the_year(day, month, year) - 1) * seconds_per_day);
-    const time_t year_seconds_since_epoch =
-        ((year - epoch_start_year) * seconds_per_day * usual_days_per_year) +
-        (((year - (epoch_start_year - 1)) / years_between_leapyears) *
-         seconds_per_day);
-
-    return year_seconds_since_epoch + second_of_year;
+    return result;
 }
 
 } // namespace hutzn
--- src/utility/date_calculation.hpp
+++ src/utility/date_calculation.hpp
@@ -30,12 +30,12 @@ namespace hutzn
 //! number of seconds elapsed since epoch time (00:00:00 UTC on 1 January 1970)
 //! will be returned. Returns -1, when at least one parameter is not valid.
 //! @param[in] second_of_day The second at a day. Must be in range of
-//!                          [0 .. 86399].
+//!                          [0..86399].
 //! @param[in] day           Day of the month. The correct range depends on the
 //!                          month and year, but at least in [1..31].
 //! @param[in] month         Month of the year. Must stay in the range of
 //!                          [1..12].
-//! @param[in] year          Year, which must be greater than or equal to 1970.
+//! @param[in] year          Year, which must be in the range of [1970..2099].
 epoch_time_t seconds_since_epoch(const uint32_t second_of_day,
                                  const uint8_t day, const uint8_t month,
                                  const uint32_t year);
