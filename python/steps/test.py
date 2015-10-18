2c9035980c78c35903f3f988f30f0df19e5e48de
Stefan Weiser <stefan.weiser@bluewin.ch>
2015-10-18 21:02:50 +0200

    Let test step depend on build.

--- python/steps/test.py
+++ python/steps/test.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 
+import steps.build
+
 
 class TestStep(object):
     """ Runs unit and integration tests. """
 
+    def __init__(self):
+        self.buildstep = steps.build.BuildStep()
+
     def execute(self, args, path):
+        self.buildstep.execute(args, path)
+
         args.log_obj.info('Run tests...')
         args.log_obj.execute([path.unittest_bin])
         args.log_obj.execute([path.integrationtest_bin])
