b7891a24b713806e7adbe8a5e879fe7bcba03925
Stefan Weiser <stefan.weiser@bluewin.ch>
2015-11-09 20:11:13 +0100

    Split check step into smaller parts.

--- python/steps/check.py
+++ python/steps/check.py
@@ -61,21 +61,7 @@ class CheckStep(object):
         cppcheck_report_file.close()
 
         CheckStep.__fix_cppcheck_report(cppcheck_report_filename, path.project)
-
-        args.log_obj.info('Run rats...')
-        rats_report_file = open(os.path.join(path.reports, 'rats.xml'), 'w+')
-        args.log_obj.execute(['rats', '--xml', '--resultsonly', '-w', '3',
-                              src_path], rats_report_file, logger.STDOUT)
-        rats_report_file.close()
-
-        args.log_obj.info('Run vera++...')
-        vera_cmd_line = ['vera++', '--checkstyle-report',
-                         os.path.join(path.reports, 'vera++.xml')]
-        for dirpath, _, files in os.walk(lib_path):
-            for f in files:
-                if f.endswith('.cpp') or f.endswith('.hpp'):
-                    vera_cmd_line.append(os.path.join(dirpath, f))
-        args.log_obj.execute(vera_cmd_line)
+        CheckStep.__run_rats_and_vera(args, src_path, lib_path, path.reports)
 
         args.log_obj.info('Run All report files were written to ' +
                           path.reports + '.')
@@ -101,6 +87,25 @@ class CheckStep(object):
                    attr.startswith(os.path.join(project_path, 'src/unittest')):
                     errors_node.remove(error)
             tree.write(cppcheck_report_filename)
+
+    @staticmethod
+    def __run_rats_and_vera(args, src_path, lib_path, report_path):
+        ''' will produce reports for rats and vera++ '''
+
+        args.log_obj.info('Run rats...')
+        rats_report_file = open(os.path.join(report_path, 'rats.xml'), 'w+')
+        args.log_obj.execute(['rats', '--xml', '--resultsonly', '-w', '3',
+                              src_path], rats_report_file, logger.STDOUT)
+        rats_report_file.close()
+
+        args.log_obj.info('Run vera++...')
+        vera_cmd_line = ['vera++', '--checkstyle-report',
+                         os.path.join(report_path, 'vera++.xml')]
+        for dirpath, _, files in os.walk(lib_path):
+            for f in files:
+                if f.endswith('.cpp') or f.endswith('.hpp'):
+                    vera_cmd_line.append(os.path.join(dirpath, f))
+        args.log_obj.execute(vera_cmd_line)
 
     @staticmethod
     def name():
