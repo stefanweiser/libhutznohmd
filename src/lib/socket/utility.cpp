83ec41e79ddf5f01a40b444641502707676f24b8
Stefan Weiser <stefan.weiser@bluewin.ch>
2015-11-19 20:01:00 +0100

    Add comments to socket/utility.cpp.

--- src/lib/socket/utility.cpp
+++ src/lib/socket/utility.cpp
@@ -30,10 +30,13 @@ namespace hutzn
 
 int32_t close_signal_safe(const int32_t file_descriptor) noexcept(true)
 {
+    // loop until this close command is not interrupted by a signal
     int32_t result;
     do {
         result = close(file_descriptor);
     } while ((result == -1) && (errno == EINTR));
+
+    // return the result which must not be an interruption
     return result;
 }
 
@@ -41,10 +44,13 @@ int32_t accept_signal_safe(const int32_t socket_descriptor,
                            sockaddr* const address,
                            socklen_t* const size) noexcept(true)
 {
+    // loop until this accept command is not interrupted by a signal
     int32_t result;
     do {
         result = accept(socket_descriptor, address, size);
     } while ((result == -1) && (errno == EINTR));
+
+    // return the result which must not be an interruption
     return result;
 }
 
@@ -52,20 +58,20 @@ int32_t connect_signal_safe(const int32_t file_descriptor,
                             const sockaddr* const address,
                             const socklen_t size) noexcept(true)
 {
-    // Try to connect.
+    // try to connect
     int32_t result = connect(file_descriptor, address, size);
 
-    // If this fails, try to recover from this error state.
+    // try to recover from this error state when this command fails
     if ((result == -1) && (errno == EINTR)) {
 
-        // Wait till the socket gets writable.
+        // poll the socket till it gets writable
         pollfd p{file_descriptor, POLLOUT, 0};
         do {
             result = poll(&p, 1, -1);
         } while ((result == -1) && (errno == EINTR));
 
         if (result != -1) {
-            // Check the error option of the socket.
+            // check the error option of the socket
             int32_t error;
             socklen_t s = sizeof(error);
             result =
@@ -82,10 +88,13 @@ ssize_t send_signal_safe(const int32_t file_descriptor,
                          const void* const buffer, const size_t size,
                          const int32_t flags) noexcept(true)
 {
+    // loop until this send command is not interrupted by a signal
     ssize_t sent;
     do {
         sent = send(file_descriptor, buffer, size, flags);
     } while ((sent == -1) && (errno == EINTR));
+
+    // return the result which must not be an interruption
     return sent;
 }
 
@@ -93,6 +102,7 @@ ssize_t receive_signal_safe(const int32_t file_descriptor, void* const buffer,
                             const size_t size,
                             const int32_t flags) noexcept(true)
 {
+    // loop until this recv command is not interrupted by a signal
     ssize_t received;
     do {
         received = recv(file_descriptor, buffer, size, flags);
@@ -101,6 +111,7 @@ ssize_t receive_signal_safe(const int32_t file_descriptor, void* const buffer,
     if (received == -1) {
         throw std::system_error(errno, std::system_category());
     }
+    // return the result which must not be an interruption
     return received;
 }
 
@@ -109,10 +120,12 @@ sockaddr_in fill_address(const std::string& host,
 {
     sockaddr_in address;
     if (0 == inet_aton(host.c_str(), &address.sin_addr)) {
+        // specify address as unspecific when conversion fails
         address.sin_family = AF_UNSPEC;
     } else {
-        address.sin_port = htons(port);
+        // otherwise set the result
         address.sin_family = AF_INET;
+        address.sin_port = htons(port);
     }
     return address;
 }
