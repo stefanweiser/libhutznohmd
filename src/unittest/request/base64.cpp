208c15b77879771aa6d2afd20751b8d6c5c6493b
Stefan Weiser <stefan.weiser@bluewin.ch>
2015-12-07 20:52:00 +0100

    More const correctness.

--- src/unittest/request/base64.cpp
+++ src/unittest/request/base64.cpp
@@ -28,84 +28,85 @@ namespace hutzn
 
 TEST(base64, encode_empty)
 {
-    std::string base64 = encode_base64({});
-    EXPECT_EQ("", base64);
+    const std::string base64 = encode_base64({});
+    EXPECT_STREQ("", base64.c_str());
 }
 
 TEST(base64, encode_space)
 {
-    std::string base64 = encode_base64({' '});
-    EXPECT_EQ("IA==", base64);
+    const std::string base64 = encode_base64({' '});
+    EXPECT_STREQ("IA==", base64.c_str());
 }
 
 TEST(base64, encode_hello_world)
 {
-    std::string base64 = encode_base64(
+    const std::string base64 = encode_base64(
         {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!'});
-    EXPECT_EQ("SGVsbG8gV29ybGQh", base64);
+    EXPECT_STREQ("SGVsbG8gV29ybGQh", base64.c_str());
 }
 
 TEST(base64, encode_pangram)
 {
-    std::string base64 = encode_base64(
+    const std::string base64 = encode_base64(
         {'P', 'u', 'b', 'l', 'i', 'c', ' ', 'j', 'u', 'n', 'k', ' ', 'd', 'w',
          'a', 'r', 'v', 'e', 's', ' ', 'h', 'u', 'g', ' ', 'm', 'y', ' ', 'q',
          'u', 'a', 'r', 't', 'z', ' ', 'f', 'o', 'x', '.'});
-    EXPECT_EQ("UHVibGljIGp1bmsgZHdhcnZlcyBodWcgbXkgcXVhcnR6IGZveC4=", base64);
+    EXPECT_STREQ("UHVibGljIGp1bmsgZHdhcnZlcyBodWcgbXkgcXVhcnR6IGZveC4=",
+                 base64.c_str());
 }
 
 TEST(base64, decode_empty)
 {
-    std::vector<uint8_t> decoded_data = decode_base64("");
+    const std::vector<uint8_t> decoded_data = decode_base64("");
     EXPECT_EQ(std::vector<uint8_t>(), decoded_data);
 }
 
 TEST(base64, decode_space)
 {
-    std::vector<uint8_t> decoded_data = decode_base64("IA==");
-    std::vector<uint8_t> data{' '};
+    const std::vector<uint8_t> decoded_data = decode_base64("IA==");
+    const std::vector<uint8_t> data{' '};
     EXPECT_EQ(data, decoded_data);
 }
 
 TEST(base64, decode_double_space)
 {
-    std::vector<uint8_t> decoded_data = decode_base64("ICA=");
-    std::vector<uint8_t> data{' ', ' '};
+    const std::vector<uint8_t> decoded_data = decode_base64("ICA=");
+    const std::vector<uint8_t> data{' ', ' '};
     EXPECT_EQ(data, decoded_data);
 }
 
 TEST(base64, decode_space_without_pad)
 {
-    std::vector<uint8_t> decoded_data = decode_base64("IA");
-    std::vector<uint8_t> data{' '};
+    const std::vector<uint8_t> decoded_data = decode_base64("IA");
+    const std::vector<uint8_t> data{' '};
     EXPECT_EQ(data, decoded_data);
 }
 
 TEST(base64, decode_char62_char63)
 {
-    std::vector<uint8_t> decoded_data = decode_base64("+/+/");
-    std::vector<uint8_t> data{251, 255, 191};
+    const std::vector<uint8_t> decoded_data = decode_base64("+/+/");
+    const std::vector<uint8_t> data{251, 255, 191};
     EXPECT_EQ(data, decoded_data);
 }
 
 TEST(base64, decode_alternative_char62_char63)
 {
-    std::vector<uint8_t> decoded_data = decode_base64("-_-_");
-    std::vector<uint8_t> data{251, 255, 191};
+    const std::vector<uint8_t> decoded_data = decode_base64("-_-_");
+    const std::vector<uint8_t> data{251, 255, 191};
     EXPECT_EQ(data, decoded_data);
 }
 
 TEST(base64, decode_hello_world)
 {
-    std::vector<uint8_t> decoded_data = decode_base64("SGVsbG8gV29ybGQh");
-    std::vector<uint8_t> data{'H', 'e', 'l', 'l', 'o', ' ',
-                              'W', 'o', 'r', 'l', 'd', '!'};
+    const std::vector<uint8_t> decoded_data = decode_base64("SGVsbG8gV29ybGQh");
+    const std::vector<uint8_t> data{'H', 'e', 'l', 'l', 'o', ' ',
+                                    'W', 'o', 'r', 'l', 'd', '!'};
     EXPECT_EQ(data, decoded_data);
 }
 
 TEST(base64, decode_errornous)
 {
-    std::vector<uint8_t> decoded_data = decode_base64("I");
+    const std::vector<uint8_t> decoded_data = decode_base64("I");
     EXPECT_EQ(std::vector<uint8_t>(), decoded_data);
 }
 
