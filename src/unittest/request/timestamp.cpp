a4322e61c3703fcffe1756965f6e0dee810069bd
Stefan Weiser <stefan.weiser@bluewin.ch>
2015-12-16 21:09:17 +0100

    Remove fixture from timestamp tests.

--- src/unittest/request/timestamp.cpp
+++ src/unittest/request/timestamp.cpp
@@ -26,355 +26,311 @@ using namespace testing;
 namespace hutzn
 {
 
-namespace
-{
-
-using string_index_pair = std::pair<std::string, size_t>;
-
-int32_t get_char(void* handle)
-{
-    string_index_pair* p = static_cast<string_index_pair*>(handle);
-    if (p->second < p->first.size()) {
-        return static_cast<uint8_t>(p->first[p->second++]);
-    }
-    return -1;
-}
-
-int32_t peek_char(void* handle)
-{
-    string_index_pair* p = static_cast<string_index_pair*>(handle);
-    if (p->second < p->first.size()) {
-        return static_cast<uint8_t>(p->first[p->second]);
-    }
-    return -1;
-}
-
-class fixture
-{
-public:
-    explicit fixture(const std::string& str);
-    time_t parse();
-
-    std::string str_;
-};
-
-fixture::fixture(const std::string& str)
-    : str_(str)
-{
-}
-
-time_t fixture::parse()
-{
-    return parse_timestamp(str_.data(), str_.size());
-}
-
-} // namespace
-
 TEST(timestamp, rfc1123_date)
 {
-    fixture f("Sun, 06 Nov 1994 08:49:37 GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Sun, 06 Nov 1994 08:49:37 GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(784111777, timestamp);
 }
 
 TEST(timestamp, rfc1123_tolerant_date)
 {
-    fixture f("Sun,06Nov1994   08:49:37GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Sun,06Nov1994   08:49:37GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(784111777, timestamp);
 }
 
 TEST(timestamp, rfc1123_erroneous_month)
 {
-    fixture f("Sun, 06 Non 1994 08:49:37 GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Sun, 06 Non 1994 08:49:37 GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(-1, timestamp);
 }
 
 TEST(timestamp, rfc1123_erroneous_hour)
 {
-    fixture f("Sun, 06 Nov 1994 24:49:37 GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Sun, 06 Nov 1994 24:49:37 GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(-1, timestamp);
 }
 
 TEST(timestamp, rfc1123_erroneous_minute)
 {
-    fixture f("Sun, 06 Nov 1994 08:60:37 GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Sun, 06 Nov 1994 08:60:37 GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(-1, timestamp);
 }
 
 TEST(timestamp, rfc1123_erroneous_second)
 {
-    fixture f("Sun, 06 Nov 1994 08:49:60 GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Sun, 06 Nov 1994 08:49:60 GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(-1, timestamp);
 }
 
 TEST(timestamp, rfc1123_erroneous_gmt)
 {
-    fixture f("Sun, 06 Nov 1994 08:49:37 TNT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Sun, 06 Nov 1994 08:49:37 TNT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(-1, timestamp);
 }
 
 TEST(timestamp, rfc850_sunday)
 {
-    fixture f("Sunday, 14-Apr-85 00:00:00 GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Sunday, 14-Apr-85 00:00:00 GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(482284800, timestamp);
 }
 
 TEST(timestamp, rfc850_tuesday)
 {
-    fixture f("Tuesday, 14-Apr-87 00:00:00 GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Tuesday, 14-Apr-87 00:00:00 GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(545356800, timestamp);
 }
 
 TEST(timestamp, rfc850_wednesday)
 {
-    fixture f("Wednesday, 14-Apr-93 00:00:00 GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Wednesday, 14-Apr-93 00:00:00 GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(734745600, timestamp);
 }
 
 TEST(timestamp, rfc850_thursday)
 {
-    fixture f("Thursday, 14-Apr-88 00:00:00 GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Thursday, 14-Apr-88 00:00:00 GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(576979200, timestamp);
 }
 
 TEST(timestamp, rfc850_saturday)
 {
-    fixture f("Saturday, 14-Apr-90 00:00:00 GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Saturday, 14-Apr-90 00:00:00 GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(640051200, timestamp);
 }
 
 TEST(timestamp, rfc850_tolerant_date)
 {
-    fixture f("Friday  ,06-Nov-94  08:49:37GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Friday  ,06-Nov-94  08:49:37GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(784111777, timestamp);
 }
 
 TEST(timestamp, rfc850_erroneous_sunday)
 {
-    fixture f("Sunnyday,06 -  Nov   -   94  08:49:37GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Sunnyday,06 -  Nov   -   94  08:49:37GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(-1, timestamp);
 }
 
 TEST(timestamp, rfc850_erroneous_tuesday)
 {
-    fixture f("Tueday,06 -  Nov   -   94  08:49:37GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Tueday,06 -  Nov   -   94  08:49:37GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(-1, timestamp);
 }
 
 TEST(timestamp, rfc850_erroneous_wednesday)
 {
-    fixture f("Weddingday,06 -  Nov   -   94  08:49:37GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Weddingday,06 -  Nov   -   94  08:49:37GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(-1, timestamp);
 }
 
 TEST(timestamp, rfc850_erroneous_thursday)
 {
-    fixture f("Thusday,06 -  Nov   -   94  08:49:37GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Thusday,06 -  Nov   -   94  08:49:37GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(-1, timestamp);
 }
 
 TEST(timestamp, rfc850_erroneous_saturday)
 {
-    fixture f("Saturnday,06 -  Nov   -   94  08:49:37GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Saturnday,06 -  Nov   -   94  08:49:37GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(-1, timestamp);
 }
 
 TEST(timestamp, rfc850_erroneous_noday)
 {
-    fixture f("Noday, 06-Nov-94 08:49:37 GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Noday, 06-Nov-94 08:49:37 GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(-1, timestamp);
 }
 
 TEST(timestamp, rfc850_erroneous_monday)
 {
-    fixture f("Monday 06-Nov-94 08:49:37 GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Monday 06-Nov-94 08:49:37 GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(-1, timestamp);
 }
 
 TEST(timestamp, rfc850_erroneous_day)
 {
-    fixture f("Monday, 00-Nov-94 08:49:37 GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Monday, 00-Nov-94 08:49:37 GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(-1, timestamp);
 }
 
 TEST(timestamp, rfc850_erroneous_leap_day)
 {
-    fixture f("Monday, 30-Feb-96 08:49:37 GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Monday, 30-Feb-96 08:49:37 GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(-1, timestamp);
 }
 
 TEST(timestamp, rfc850_erroneous_first_hyphen)
 {
-    fixture f("Sunday, 06_Nov-94 08:49:37 GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Sunday, 06_Nov-94 08:49:37 GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(-1, timestamp);
 }
 
 TEST(timestamp, rfc850_erroneous_second_hyphen)
 {
-    fixture f("Sunday, 06-Nov_94 08:49:37 GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Sunday, 06-Nov_94 08:49:37 GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(-1, timestamp);
 }
 
 TEST(timestamp, rfc850_erroneous_year)
 {
-    fixture f("Sunday, 06-Nov-100 08:49:37 GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Sunday, 06-Nov-100 08:49:37 GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(-1, timestamp);
 }
 
 TEST(timestamp, rfc850_erroneous_gmt)
 {
-    fixture f("Sunday, 06-Nov-94 08:49:37 TNT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Sunday, 06-Nov-94 08:49:37 TNT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(-1, timestamp);
 }
 
 TEST(timestamp, asctime_date)
 {
-    fixture f("Sun Nov  6 08:49:37 1994");
-    const time_t timestamp = f.parse();
+    const std::string str = "Sun Nov  6 08:49:37 1994";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(784111777, timestamp);
 }
 
 TEST(timestamp, asctime_tolerant_date)
 {
-    fixture f("fRI Nov     6 08:49:37    1994 ");
-    const time_t timestamp = f.parse();
+    const std::string str = "fRI Nov     6 08:49:37    1994";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(784111777, timestamp);
 }
 
 TEST(timestamp, asctime_erroneous_date)
 {
-    fixture f("Mon Jul 21 03:56:0 1969");
-    const time_t timestamp = f.parse();
+    const std::string str = "Mon Jul 21 03:56:0 1969";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(-1, timestamp);
 }
 
 TEST(timestamp, jan_date)
 {
-    fixture f("Thu, 01 Jan 1970 00:00:00 GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Thu, 01 Jan 1970 00:00:00 GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(0, timestamp);
 }
 
 TEST(timestamp, feb_date)
 {
-    fixture f("Sun, 01 Feb 1970 00:00:00 GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Sun, 01 Feb 1970 00:00:00 GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(2678400, timestamp);
 }
 
 TEST(timestamp, mar_date)
 {
-    fixture f("Sun, 01 Mar 1970 00:00:00 GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Sun, 01 Mar 1970 00:00:00 GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(5097600, timestamp);
 }
 
 TEST(timestamp, apr_date)
 {
-    fixture f("Wed, 01 Apr 1970 00:00:00 GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Wed, 01 Apr 1970 00:00:00 GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(7776000, timestamp);
 }
 
 TEST(timestamp, may_date)
 {
-    fixture f("Fri, 01 May 1970 00:00:00 GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Fri, 01 May 1970 00:00:00 GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(10368000, timestamp);
 }
 
 TEST(timestamp, jun_date)
 {
-    fixture f("Mon, 01 Jun 1970 00:00:00 GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Mon, 01 Jun 1970 00:00:00 GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(13046400, timestamp);
 }
 
 TEST(timestamp, jul_date)
 {
-    fixture f("Wed, 01 Jul 1970 00:00:00 GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Wed, 01 Jul 1970 00:00:00 GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(15638400, timestamp);
 }
 
 TEST(timestamp, aug_date)
 {
-    fixture f("Sat, 01 Aug 1970 00:00:00 GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Sat, 01 Aug 1970 00:00:00 GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(18316800, timestamp);
 }
 
 TEST(timestamp, sep_date)
 {
-    fixture f("Tue, 01 Sep 1970 00:00:00 GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Tue, 01 Sep 1970 00:00:00 GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(20995200, timestamp);
 }
 
 TEST(timestamp, oct_date)
 {
-    fixture f("Thu, 01 Oct 1970 00:00:00 GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Thu, 01 Oct 1970 00:00:00 GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(23587200, timestamp);
 }
 
 TEST(timestamp, nov_date)
 {
-    fixture f("Sun, 01 Nov 1970 00:00:00 GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Sun, 01 Nov 1970 00:00:00 GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(26265600, timestamp);
 }
 
 TEST(timestamp, dec_date)
 {
-    fixture f("Thu, 01 Dec 1970 00:00:00 GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Thu, 01 Dec 1970 00:00:00 GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(28857600, timestamp);
 }
 
 TEST(timestamp, year2000_date)
 {
-    fixture f("Wed, 01 Mar 2000 00:00:00 GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = "Wed, 01 Mar 2000 00:00:00 GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(951868800, timestamp);
 }
 
 TEST(timestamp, space_before_date)
 {
-    fixture f(" Sun, 06 Nov 1994 08:49:37 GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = " Sun, 06 Nov 1994 08:49:37 GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(784111777, timestamp);
 }
 
 TEST(timestamp, tab_before_date)
 {
-    fixture f("\tSun, 06 Nov 1994 08:49:37 GMT");
-    const time_t timestamp = f.parse();
+    const std::string str = "\tSun, 06 Nov 1994 08:49:37 GMT";
+    const time_t timestamp = parse_timestamp(str.data(), str.size());
     EXPECT_EQ(784111777, timestamp);
 }
 
