0021812be9af1c24cc4bb3f42e8c5ec3bd411325
Stefan Weiser <stefan.weiser@bluewin.ch>
2015-12-16 21:14:38 +0100

    Improve timestamp documentation.

--- src/lib/request/timestamp.cpp
+++ src/lib/request/timestamp.cpp
@@ -39,13 +39,19 @@ static const int32_t begin_of_19_hundrets = 1900;
 static const int32_t end_of_19_hundrets = 1999;
 static const size_t trie_string_max_length = 16;
 
+//! Consists of the zero-based weekday number, which starts at sunday and a
+//! boolean, that is true when it is a long dayname.
 using weekday_value_type = std::tuple<int8_t, bool>;
 
+//! @brief Makes a newly created weekday trie.
+//!
+//! @param[out] max_size Maximum size of inserted strings.
+//! @return              Trie of weekday and length tuple.
 trie<weekday_value_type> make_weekday_trie(size_t& max_size)
 {
     trie<weekday_value_type> result{true};
 
-    // Filling versions and automatically calculate the maximum length.
+    // filling versions and automatically calculate the maximum length
     max_size = 0;
     static const std::vector<std::pair<const char_t* const, weekday_value_type>>
         weekdays = {std::make_pair("sun", weekday_value_type{0, false}),
@@ -72,11 +78,15 @@ trie<weekday_value_type> make_weekday_trie(size_t& max_size)
     return result;
 }
 
+//! @brief Makes a newly created month trie.
+//!
+//! @param[out] max_size Maximum size of inserted strings.
+//! @return              Trie of one-based month tuple.
 trie<int32_t> make_month_trie(size_t& max_size)
 {
     trie<int32_t> result{true};
 
-    // Filling versions and automatically calculate the maximum length.
+    // filling versions and automatically calculate the maximum length
     max_size = 0;
     static const std::vector<std::pair<const char_t* const, int32_t>> months = {
         std::make_pair("jan", 1),  std::make_pair("feb", 2),
@@ -94,31 +104,64 @@ trie<int32_t> make_month_trie(size_t& max_size)
     return result;
 }
 
+//! @brief Parses a time format and returns the second of the day.
+//!
+//! Expects that the time is a colon seperated list of hour, minute and second.
+//! Returns -1 if an error is encountered.
+//! @param[in,out] data      Points to the begin of the time string.
+//! @param[in,out] remaining Number of bytes remaining in the data buffer.
+//! @return                  Second of the day.
 int32_t parse_time(const char_t*& data, size_t& remaining)
 {
+    // expecting number which is the hour
     const int32_t hour = parse_unsigned_integer<int32_t>(data, remaining);
+
+    // skip optional whitespace
     skip_whitespace(data, remaining);
+
+    // check whether this hour is valid and a colon is ahead
     if ((!check_range<int32_t, 0, hours_per_day - 1>(hour)) ||
         ((*data) != ':')) {
         return -1;
     }
+
+    // skip colon and optional whitespace
     skip_one_character(data, remaining);
     skip_whitespace(data, remaining);
+
+    // expecting number which is the minute
     const int32_t minute = parse_unsigned_integer<int32_t>(data, remaining);
+
+    // skip optional whitespace
     skip_whitespace(data, remaining);
+
+    // check whether this minute is valid and a colon is ahead
     if ((!check_range<int32_t, 0, minutes_per_hour - 1>(minute)) ||
         ((*data) != ':')) {
         return -1;
     }
+
+    // skip colon and optional whitespace
     skip_one_character(data, remaining);
     skip_whitespace(data, remaining);
+
+    // expecting number which is the second
     const int32_t second = parse_unsigned_integer<int32_t>(data, remaining);
+
+    // check whether this second is valid
     if (!check_range<int32_t, 0, seconds_per_minute - 1>(second)) {
         return -1;
     }
+
+    // calculate result
     return (seconds_per_minute * ((minutes_per_hour * hour) + minute)) + second;
 }
 
+//! @brief Converts a month string into its number.
+//!
+//! @param[in,out] data      Points to the begin of the month string.
+//! @param[in,out] remaining Number of bytes remaining in the data buffer.
+//! @return                  1-based month of the year.
 int32_t parse_month(const char_t*& data, size_t& remaining)
 {
     static size_t maximum_month_length = 0;
@@ -130,6 +173,13 @@ int32_t parse_month(const char_t*& data, size_t& remaining)
     return result.value();
 }
 
+//! @brief Checks if the rest of the data begins with 'gmt' in a case
+//! insensitive way.
+//!
+//! @param[in,out] data      Points to the begin of the string.
+//! @param[in,out] remaining Number of bytes remaining in the data buffer.
+//! @return                  True if the string begins with 'gmt' and false when
+//!                          not.
 bool parse_gmt(const char_t*& data, size_t& remaining)
 {
     static const size_t max_gmt_length_ = 3;
@@ -144,6 +194,11 @@ bool parse_gmt(const char_t*& data, size_t& remaining)
     return result;
 }
 
+//! @brief Parses a RFC 1123 compatible timestamp and returns it in epoch time.
+//!
+//! @param[in,out] data      Points to the begin of the string.
+//! @param[in,out] remaining Number of bytes remaining in the data buffer.
+//! @return                  Number of seconds since epoch.
 time_t parse_rfc1123_date_time(const char_t*& data, size_t& remaining)
 {
     skip_one_character(data, remaining);
@@ -172,6 +227,11 @@ time_t parse_rfc1123_date_time(const char_t*& data, size_t& remaining)
         static_cast<uint8_t>(month), static_cast<uint16_t>(year));
 }
 
+//! @brief Parses a RFC 850 compatible timestamp and returns it in epoch time.
+//!
+//! @param[in,out] data      Points to the begin of the string.
+//! @param[in,out] remaining Number of bytes remaining in the data buffer.
+//! @return                  Number of seconds since epoch.
 time_t parse_rfc850_date_time(const char_t*& data, size_t& remaining)
 {
     skip_whitespace(data, remaining);
@@ -198,6 +258,7 @@ time_t parse_rfc850_date_time(const char_t*& data, size_t& remaining)
     data++;
     remaining--;
 
+    // RFC 850 uses short year numbers
     const int32_t year =
         begin_of_19_hundrets + parse_unsigned_integer<int32_t>(data, remaining);
     if (!check_range<int32_t, begin_of_19_hundrets, end_of_19_hundrets>(year)) {
@@ -217,6 +278,11 @@ time_t parse_rfc850_date_time(const char_t*& data, size_t& remaining)
         static_cast<uint8_t>(month), static_cast<uint16_t>(year));
 }
 
+//! @brief Parses a ASCII timestamp and returns it in epoch time.
+//!
+//! @param[in,out] data      Points to the begin of the string.
+//! @param[in,out] remaining Number of bytes remaining in the data buffer.
+//! @return                  Number of seconds since epoch.
 time_t parse_asctime_date_time(const char_t*& data, size_t& remaining)
 {
     skip_whitespace(data, remaining);
@@ -239,6 +305,11 @@ time_t parse_asctime_date_time(const char_t*& data, size_t& remaining)
         static_cast<uint8_t>(month), static_cast<uint16_t>(year));
 }
 
+//! @brief Parses a weekday from a string and return its number expression.
+//!
+//! @param[in,out] data      Points to the begin of the string.
+//! @param[in,out] remaining Number of bytes remaining in the data buffer.
+//! @return                  Weekday and boolean, that indicates a long format.
 weekday_value_type parse_weekday(const char_t*& data, size_t& remaining)
 {
     static size_t maximum_weekday_length = 0;
@@ -260,11 +331,15 @@ time_t parse_timestamp(const char_t* data, size_t length)
     int32_t weekday = -1;
     int32_t is_long_format = false;
     std::tie(weekday, is_long_format) = parse_weekday(data, length);
+
     if (is_long_format) {
+        // RFC 850 begins with a long weekday
         return parse_rfc850_date_time(data, length);
     } else if (((*data) == ' ') || ((*data) == '\t')) {
+        // ASCII timestamps begin with a short weekday followed by a whitespace
         return parse_asctime_date_time(data, length);
     } else {
+        // in RFC 1123 the weekday is followed by a comma
         return parse_rfc1123_date_time(data, length);
     }
 }
